784858e62f922d309effd059ed04618fa55b38cf
Jeff Loiola <jloiola@mmitnetwork.com>
2022-06-13 19:14:59 -0400

    wip; (snuck in vscode too)... works at all with scopes

--- backend/app/routes/change_log.py
+++ backend/app/routes/change_log.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Security
 from bson import ObjectId
 
 from backend.common.models.change_log import ChangeLog
 from backend.common.models.user import User
 from backend.app.utils.user import get_current_user
-
+from backend.app.utils.auth0 import auth0
 
 router = APIRouter(
     prefix="/change-log",
@@ -15,7 +15,7 @@ router = APIRouter(
 @router.get("/{id}", response_model=list[ChangeLog])
 async def get_changes_for_id(
     id: str,
-    current_user: User = Depends(get_current_user),
+    current_user = Security(auth0.get_current_user, scopes=["profile", "asd"]),
 ) -> list[ChangeLog]:
     logs = (
         await ChangeLog.find(ChangeLog.target_id == ObjectId(id)).sort("id").to_list()
