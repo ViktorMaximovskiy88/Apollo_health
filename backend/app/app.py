aa11672e0ff97df1cb478480d6f3dc601b26d07a
Jeff Loiola <jloiola@mmitnetwork.com>
2022-06-21 13:52:36 -0400

    rm dead code

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 from typing import Any
 from fastapi import FastAPI, Request, status
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from backend.app.scripts.add_user import create_admin_user
 from backend.common.db.init import init_db
 from backend.common.db.migrations import run_migrations
@@ -33,35 +32,7 @@ async def app_init():
         user, plain_pass = await create_admin_user()
         print(f"Created admin user with email: {user}, password: {plain_pass}")
 
-
-template_dir = Path(__file__).parent.joinpath("templates")
-templates = Jinja2Templates(directory=template_dir)
 frontend_build_dir = Path(__file__).parent.joinpath("../../frontend/build").resolve()
-
-
-@app.get("/login", response_class=HTMLResponse, tags=["Auth"])
-async def login_page(request: Request):
-    error = request.query_params.get("error")
-    email = request.query_params.get("email", "")
-    html = templates.TemplateResponse(
-        "login.html", {"request": request, "error": error, "email": email}
-    )
-    return html
-
-
-@app.middleware("http")
-async def spa_routing(request: Request, call_next: Any):
-    response = await call_next(request)
-
-    if (
-        response.status_code == status.HTTP_404_NOT_FOUND
-        and not request.url.path.startswith("/api")
-    ):
-        with open(frontend_build_dir.joinpath("index.html")) as file:
-            return HTMLResponse(file.read())
-
-    return response
-
 
 app.add_middleware(GZipMiddleware)
 
@@ -74,4 +45,17 @@ app.include_router(site_scrape_tasks.router, prefix=prefix)
 app.include_router(documents.router, prefix=prefix)
 app.include_router(content_extraction_tasks.router, prefix=prefix)
 app.include_router(proxies.router, prefix=prefix)
+
+@app.middleware("http")
+async def spa_routing(request: Request, call_next: Any):
+    response = await call_next(request)
+    print(response)
+    if (
+        response.status_code == status.HTTP_404_NOT_FOUND
+        and not request.url.path.startswith("/api")
+    ):
+        return RedirectResponse(url="/")
+
+    return response
+
 app.mount("/", StaticFiles(directory=frontend_build_dir, html=True), name="static")
