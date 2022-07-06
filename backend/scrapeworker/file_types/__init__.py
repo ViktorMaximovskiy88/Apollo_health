14f190101ca0f500887b3dbb59d660597c3bd7e6
Jeff Loiola <jloiola@mmitnetwork.com>
2022-07-06 13:55:04 -0400

    mimetypes ...

--- backend/scrapeworker/file_types/__init__.py
+++ backend/scrapeworker/file_types/__init__.py
@@ -1,17 +1,32 @@
-import pathlib
+import magic
+import mimetypes
 from backend.scrapeworker.file_types import docx, xlsx, pdf
 
 __all__ = ["docx", "xlsx", "pdf"]
 
+# TODO ... not as accurate as id like...
+# mapper = mimetypes.MimeTypes().types_map_inv
+mapper = {
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": ".xlsx",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",
+    "application/pdf": ".pdf",
+}
+
+
+def guess_extension(file_path: str):
+    mime = magic.Magic(mime=True)
+    mime_type = mime.from_file(file_path)
+    return mapper[mime_type]
+
 
 async def parse_by_type(file_path: str, url):
-    # TODO file ext from content header ...
-    file_extension = pathlib.Path(file_path).suffix
-    if file_extension == "pdf":
+    file_extension = guess_extension(file_path)
+    print(f"{file_extension} ****** {file_path} {url}")
+    if file_extension == ".pdf":
         return await pdf.parse_metadata(file_path, url)
-    elif file_extension == "docx":
+    elif file_extension == ".docx":
         return await docx.parse_metadata(file_path, url)
-    elif file_extension == "xlsx":
+    elif file_extension == ".xlsx":
         return await xlsx.parse_metadata(file_path, url)
     else:
         raise NotImplementedError(f"no parse for file ext {file_extension}")
