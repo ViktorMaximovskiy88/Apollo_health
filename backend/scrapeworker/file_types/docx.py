ec4b4932c475492d94337ee635858a3a96df108c
Jeff Loiola <jloiola@mmitnetwork.com>
2022-07-06 11:47:32 -0400

    docx to text

--- backend/scrapeworker/file_types/docx.py
+++ backend/scrapeworker/file_types/docx.py
@@ -1,2 +1,10 @@
-def parse_metadata():
+import docx2txt
+
+
+def docx_to_text(temp_path: str):
+    text = docx2txt.process(temp_path)
+    return text
+
+
+async def parse_metadata(temp_path, url) -> dict[str, str]:
     pass
--- backend/scrapeworker/file_types/docx_test.py
+++ backend/scrapeworker/file_types/docx_test.py
@@ -1,19 +1,19 @@
 import os
 import pytest
 import aiofiles
-from backend.scrapeworker.file_types.xlsx import xlsx_to_text
+from backend.scrapeworker.file_types.docx import docx_to_text
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 fixture_path = os.path.join(current_path, "__fixtures__")
 
 
 @pytest.mark.asyncio
-async def test_xlsx_to_text():
-    file_path = os.path.join(fixture_path, "test.xlsx")
-    expected_path = os.path.join(fixture_path, "test_xlsx.txt")
+async def test_docx_to_text():
+    file_path = os.path.join(fixture_path, "test.docx")
+    expected_path = os.path.join(fixture_path, "test_docx.txt")
 
     async with aiofiles.open(expected_path, mode="r") as file:
         expected_text = await file.read()
 
-    text = xlsx_to_text(file_path)
+    text = docx_to_text(file_path)
     assert text == expected_text
