a0729d0c38eab5c9d968b6d31d7c31a0d3902601
Andrew Horn <ahorn@mmitnetwork.com>
2022-07-06 12:05:00 -0500

    abstracted parts of UrlInput into own components

--- frontend/src/features/sites/UrlFormField.tsx
+++ frontend/src/features/sites/UrlFormField.tsx
@@ -8,16 +8,20 @@ import { useState } from 'react';
 import { ActiveUrlResponse, BaseUrl, Site } from './types';
 import { fetchWithAuth } from '../../app/base-api';
 import { FormInstance } from 'antd/lib/form/Form';
+import { FormListFieldData } from 'antd/lib/form/FormList';
 
-export function UrlFormFields(props: {
+function UrlInput(props: {
   initialValues?: Site;
+  key: number;
+  name: number;
+  field: { fieldKey?: number };
   form: FormInstance;
 }) {
+  const currentSite = props.initialValues ? props.initialValues._id : '';
+
   const [urlValidation, setUrlValidation] = useState<{
     [id: string]: ActiveUrlResponse;
   }>({});
-
-  const currentSite = props.initialValues ? props.initialValues._id : '';
 
   async function validateUrl(key: number, value: string) {
     const checkUrl = encodeURIComponent(value);
@@ -65,12 +69,74 @@ export function UrlFormFields(props: {
         </p>
       );
     }
-
-    return undefined;
+    return null;
   }
 
+  return (
+    <Form.Item
+      className="grow mb-0"
+      hasFeedback
+      help={createErrorMessage(props.key)}
+      {...props.field}
+      label="URL"
+      name={[props.name, 'url']}
+      rules={[
+        {
+          required: true,
+          type: 'url',
+        },
+        {
+          validator: (_, value) => validateUrl(props.key, value),
+        },
+      ]}
+      validateTrigger="onBlur"
+    >
+      <Input.TextArea autoSize={{ minRows: 1, maxRows: 3 }} />
+    </Form.Item>
+  );
+}
+
+interface LabelInputPropTypes {
+  name: number;
+  field: { fieldKey?: number };
+}
+function LabelInput({ name, field }: LabelInputPropTypes) {
+  return (
+    <Form.Item {...field} name={[name, 'name']} label="Label" className="mb-0">
+      <Input />
+    </Form.Item>
+  );
+}
+
+interface StatusSelectPropTypes {
+  name: number;
+  field: { fieldKey?: number };
+}
+function StatusSelect({ name, field }: StatusSelectPropTypes) {
+  return (
+    <Form.Item
+      {...field}
+      name={[name, 'status']}
+      label="Status"
+      className="mb-0"
+    >
+      <Select
+        options={[
+          { value: 'ACTIVE', label: 'Active' },
+          { value: 'INACTIVE', label: 'Inactive' },
+        ]}
+      />
+    </Form.Item>
+  );
+}
+
+interface LinkButtonPropTypes {
+  form: FormInstance;
+  index: number;
+}
+function LinkButton({ form, index }: LinkButtonPropTypes) {
   const hasError = (fieldName: string, fieldIndex: number): boolean => {
-    const errors = props.form.getFieldsError();
+    const errors = form.getFieldsError();
     const fieldErrors = errors.filter(({ errors, name }) => {
       const [currentFieldName, currentFieldIndex] = name;
       if (!currentFieldName) return false;
@@ -80,6 +146,49 @@ export function UrlFormFields(props: {
     });
     return fieldErrors.length > 0;
   };
+
+  return (
+    <Form.Item label=" " shouldUpdate className="mb-0">
+      {() => (
+        <Button
+          className="p-0 focus:border focus:border-offset-2 focus:border-blue-500"
+          href={form.getFieldValue('base_urls')[index].url}
+          disabled={hasError('base_urls', index)}
+          type="link"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <LinkOutlined className="text-gray-500 hover:text-blue-500 focus:text-blue-500" />
+        </Button>
+      )}
+    </Form.Item>
+  );
+}
+
+interface RemoveButtonPropTypes {
+  fields: FormListFieldData[];
+  remove: (index: number | number[]) => void;
+  name: number;
+}
+function RemoveButton({ fields, remove, name }: RemoveButtonPropTypes) {
+  if (fields.length < 2) {
+    return null;
+  }
+  return (
+    <Form.Item label=" " className="mb-0">
+      <MinusCircleOutlined
+        className="text-gray-500"
+        onClick={() => remove(name)}
+      />
+    </Form.Item>
+  );
+}
+
+interface UrlFormFieldPropTypes {
+  initialValues?: Site;
+  form: FormInstance;
+}
+export function UrlFormFields({ initialValues, form }: UrlFormFieldPropTypes) {
   return (
     <Form.List name="base_urls">
       {(fields, { add, remove }, { errors }) => (
@@ -87,71 +196,17 @@ export function UrlFormFields(props: {
           {fields.map(({ key, name, ...field }, index) => (
             <Form.Item key={key} className="mb-2" {...field}>
               <Input.Group className="space-x-2 flex">
-                <Form.Item
-                  className="grow mb-0"
-                  hasFeedback
-                  help={createErrorMessage(key)}
-                  {...field}
-                  label="URL"
-                  name={[name, 'url']}
-                  rules={[
-                    {
-                      required: true,
-                      type: 'url',
-                    },
-                    {
-                      validator: (_, value) => validateUrl(key, value),
-                    },
-                  ]}
-                  validateTrigger="onBlur"
-                >
-                  <Input.TextArea autoSize={{ minRows: 1, maxRows: 3 }} />
-                </Form.Item>
-
-                <Form.Item
-                  {...field}
-                  name={[name, 'name']}
-                  label="Label"
-                  className="mb-0"
-                >
-                  <Input />
-                </Form.Item>
-
-                <Form.Item
-                  {...field}
-                  name={[name, 'status']}
-                  label="Status"
-                  className="mb-0"
-                >
-                  <Select
-                    options={[
-                      { value: 'ACTIVE', label: 'Active' },
-                      { value: 'INACTIVE', label: 'Inactive' },
-                    ]}
-                  />
-                </Form.Item>
-                <Form.Item label=" " shouldUpdate className="mb-0">
-                  {() => (
-                    <Button
-                      className="p-0 focus:border focus:border-offset-2 focus:border-blue-500"
-                      href={props.form.getFieldValue('base_urls')[index].url}
-                      disabled={hasError('base_urls', index)}
-                      type="link"
-                      target="_blank"
-                      rel="noreferrer noopener"
-                    >
-                      <LinkOutlined className="text-gray-500 hover:text-blue-500 focus:text-blue-500" />
-                    </Button>
-                  )}
-                </Form.Item>
-                {fields.length > 1 ? (
-                  <Form.Item label=" " className="mb-0">
-                    <MinusCircleOutlined
-                      className="text-gray-500"
-                      onClick={() => remove(name)}
-                    />
-                  </Form.Item>
-                ) : null}
+                <UrlInput
+                  initialValues={initialValues}
+                  key={key}
+                  name={name}
+                  field={field}
+                  form={form}
+                />
+                <LabelInput name={name} field={field} />
+                <StatusSelect name={name} field={field} />
+                <LinkButton form={form} index={index} />
+                <RemoveButton fields={fields} remove={remove} name={name} />
               </Input.Group>
             </Form.Item>
           ))}
