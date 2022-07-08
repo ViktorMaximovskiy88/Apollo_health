db0ee4f72b48aef29a1dbdae592942402defb25a
Andrew Horn <ahorn@mmitnetwork.com>
2022-07-08 11:14:05 -0500

    updated migration files so they don't wipe values that already exist in output_docs

--- backend/common/db/migrations/20220707004353_add_status_to_site.py
+++ backend/common/db/migrations/20220707004353_add_status_to_site.py
@@ -6,6 +6,8 @@ from backend.common.core.enums import TaskStatus, SiteStatus
 class Forward:
     @iterative_migration()
     async def add_status_to_site(self, input_document: Site, output_document: Site):
+        if output_document.status:
+            return
         if input_document.last_run_status == TaskStatus.FAILED:
             output_document.status = SiteStatus.QUALITY_HOLD
         else:
