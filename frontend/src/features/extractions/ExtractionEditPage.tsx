0c11f9a30fef6c9d4ad9900f891ef902cb28e932
Andrew Horn <ahorn@mmitnetwork.com>
2022-07-19 12:56:05 -0500

    added dependency to dependency array

--- frontend/src/features/extractions/ExtractionEditPage.tsx
+++ frontend/src/features/extractions/ExtractionEditPage.tsx
@@ -1,6 +1,6 @@
 import ReactDataGrid from '@inovua/reactdatagrid-community';
 import Title from 'antd/lib/typography/Title';
-import NumberFilter from '@inovua/reactdatagrid-community/NumberFilter'
+import NumberFilter from '@inovua/reactdatagrid-community/NumberFilter';
 import { useParams } from 'react-router-dom';
 import { useGetExtractionTaskQuery, useLazyGetExtractionTaskResultsQuery } from './extractionsApi';
 import { useCallback } from 'react';
@@ -11,46 +11,54 @@ export function ExtractionEditPage() {
   const [getResultsFn] = useLazyGetExtractionTaskResultsQuery();
   const { data: extractionTask } = useGetExtractionTaskQuery(extractionId);
 
-  const loadData = useCallback(async (tableInfo: any) => {
-    let sortInfo: TypeSingleSortInfo[] = tableInfo.sortInfo
-    sortInfo = sortInfo.map((info) => {
-      if (info.name.startsWith('t_')) {
-        return { ...info, name: `translation.${info.name.replace(/^t_/, '')}` }
-      } else if (info.name === 'page' || info.name == 'row') {
-        return info
-      } else {
-        return { ...info, name: `result.${info.name}` }
-      }
-    });
-    let filterValue: TypeSingleFilterValue[] = tableInfo.filterValue
-    filterValue = filterValue.map((filter) => {
-      if (filter.name.startsWith('t_')) {
-        return { ...filter,  name: `translation.${filter.name.replace(/^t_/, '')}` }
-      } else if (filter.name === 'page' || filter.name == 'row') {
-        return filter
-      } else {
-        return { ...filter, name: `result.${filter.name}` }
-      }
-    });
-
-    const { data } = await getResultsFn({ id: extractionId, ...tableInfo, sortInfo, filterValue });
-    const extractions = data?.data || [];
-    const count = data?.total || 0;
-    const formattedExtractions = extractions.map(({ page, row, result, translation }) => {
-      const translatedPrefixedKeys: any = {}
-      if (translation) {
-        for (const [key, value] of Object.entries(translation)) {
-          translatedPrefixedKeys[`t_${key}`] = value
+  const loadData = useCallback(
+    async (tableInfo: any) => {
+      let sortInfo: TypeSingleSortInfo[] = tableInfo.sortInfo;
+      sortInfo = sortInfo.map((info) => {
+        if (info.name.startsWith('t_')) {
+          return { ...info, name: `translation.${info.name.replace(/^t_/, '')}` };
+        } else if (info.name === 'page' || info.name == 'row') {
+          return info;
+        } else {
+          return { ...info, name: `result.${info.name}` };
         }
-      }
-      return { page, row, ...result, ...translatedPrefixedKeys }
-    });
-    return { data: formattedExtractions, count }
-  }, [getResultsFn])
-  
+      });
+      let filterValue: TypeSingleFilterValue[] = tableInfo.filterValue;
+      filterValue = filterValue.map((filter) => {
+        if (filter.name.startsWith('t_')) {
+          return { ...filter, name: `translation.${filter.name.replace(/^t_/, '')}` };
+        } else if (filter.name === 'page' || filter.name == 'row') {
+          return filter;
+        } else {
+          return { ...filter, name: `result.${filter.name}` };
+        }
+      });
+
+      const { data } = await getResultsFn({
+        id: extractionId,
+        ...tableInfo,
+        sortInfo,
+        filterValue,
+      });
+      const extractions = data?.data || [];
+      const count = data?.total || 0;
+      const formattedExtractions = extractions.map(({ page, row, result, translation }) => {
+        const translatedPrefixedKeys: any = {};
+        if (translation) {
+          for (const [key, value] of Object.entries(translation)) {
+            translatedPrefixedKeys[`t_${key}`] = value;
+          }
+        }
+        return { page, row, ...result, ...translatedPrefixedKeys };
+      });
+      return { data: formattedExtractions, count };
+    },
+    [extractionId, getResultsFn]
+  );
+
   if (!extractionTask) return null;
 
-  const header = extractionTask.header || []
+  const header = extractionTask.header || [];
 
   const translatedColumns = [
     {
@@ -64,8 +72,16 @@ export function ExtractionEditPage() {
       name: 't_code',
       group: 'translated',
       render: ({ value }: { value: string }) => {
-        return <a target="_blank" href={`https://mor.nlm.nih.gov/RxNav/search?searchBy=RXCUI&searchTerm=${value}`}>{value}</a>
-      }
+        return (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://mor.nlm.nih.gov/RxNav/search?searchBy=RXCUI&searchTerm=${value}`}
+          >
+            {value}
+          </a>
+        );
+      },
     },
     {
       header: 'Tier',
@@ -94,47 +110,49 @@ export function ExtractionEditPage() {
       name: 'page',
       group: 'raw',
       type: 'number',
-      filterEditor: NumberFilter 
+      filterEditor: NumberFilter,
     },
     {
       header: 'Row',
       name: 'row',
       group: 'raw',
       type: 'number',
-      filterEditor: NumberFilter 
-    }
-  ]
+      filterEditor: NumberFilter,
+    },
+  ];
   header.forEach((h) => {
     columns.push({
-        header: h,
-        name: h,
-        group: 'raw',
-    })
-  })
+      header: h,
+      name: h,
+      group: 'raw',
+    });
+  });
   translatedColumns.forEach((c) => {
     columns.push({
       group: 'translated',
-      ...c
-    })
-  })
-    
+      ...c,
+    });
+  });
+
   const groups = [
     { header: 'Raw', name: 'raw' },
-    { header: 'Translated', name: 'translated' }
-  ]
-  
+    { header: 'Translated', name: 'translated' },
+  ];
+
   const fixedFilters: any[] = [
     { name: 'page', operator: 'eq', type: 'number', value: null },
     { name: 'row', operator: 'eq', type: 'number', value: null },
     { name: 't_tier', operator: 'eq', type: 'number', value: null },
     { name: 't_name', operator: 'contains', type: 'string', value: '' },
     { name: 't_code', operator: 'contains', type: 'string', value: '' },
-  ]
-  const defaultFilterValue = header.map((name) => ({ name, operator: 'contains', type: 'string', value: '' })).concat(fixedFilters)
+  ];
+  const defaultFilterValue = header
+    .map((name) => ({ name, operator: 'contains', type: 'string', value: '' }))
+    .concat(fixedFilters);
   const defaultSortInfo = [
     { name: 'page', dir: 1 as 1 | -1 | 0 },
-    { name: 'row', dir: 1 as 1 | -1 | 0 }
-  ]
+    { name: 'row', dir: 1 as 1 | -1 | 0 },
+  ];
 
   return (
     <>
