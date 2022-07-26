f2f48d6355193f1794d95bcef10b9061fdcd9122
Ali Fardos <afardos@mmitnetwork.com>
2022-07-26 12:25:36 -0400

    Removed unneeded code

--- frontend/src/features/sites/SiteCreatePage.tsx
+++ frontend/src/features/sites/SiteCreatePage.tsx
@@ -9,9 +9,8 @@ export function SiteCreatePage() {
   const navigate = useNavigate();
 
   async function tryAddSite(site: Partial<Site>) {
-    console.log(site)
     await addSite(site);
-    // navigate('..');
+    navigate('..');
   }
 
   return (
