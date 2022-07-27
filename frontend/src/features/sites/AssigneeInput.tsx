8b69af5abc2873eb687a9d6a0ec5620a0c31853f
Andrew Horn <ahorn@mmitnetwork.com>
2022-07-27 09:56:04 -0500

    changed AssigneeInput to use a select instead of autocomplete element to simplify code, make functional in more cases

--- frontend/src/features/sites/AssigneeInput.tsx
+++ frontend/src/features/sites/AssigneeInput.tsx
@@ -1,14 +1,16 @@
-import { Form, Input, AutoComplete } from 'antd';
-import { FormInstance } from 'antd/lib/form/Form';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
+import { Form, Select } from 'antd';
 import { User } from '../users/types';
 import { useGetUsersQuery } from '../users/usersApi';
+
+const { Option } = Select;
 
 interface OptionType {
   label: string;
   value: string | null;
 }
-const useOptions = (users?: User[]): OptionType[] => {
+const useOptions = (): OptionType[] => {
+  const { data: users } = useGetUsersQuery();
   return useMemo(() => {
     const options: OptionType[] =
       users?.map((user: User) => ({ label: user.full_name, value: user._id })) ?? [];
@@ -16,93 +18,38 @@ const useOptions = (users?: User[]): OptionType[] => {
     return options;
   }, [users]);
 };
-const useNameState = (
-  options: OptionType[],
-  currentAssigneeId: string
-): [string | null, (name: string | null) => void] => {
-  const initialAssigneeName = options.find((option) => currentAssigneeId === option.value);
-  const [name, setName] = useState<string | null>(
-    initialAssigneeName?.label === 'Unassigned' ? null : initialAssigneeName?.label ?? null
-  );
-  return [name, setName];
-};
-interface KeepFieldsInSyncTypes {
-  currentAssigneeId: string;
-  options: OptionType[];
-  setName: (name: string | null) => void;
-}
-const useKeepFieldsInSync = ({ options, currentAssigneeId, setName }: KeepFieldsInSyncTypes) => {
-  useEffect(() => {
-    const option = options.find((option: OptionType) => option.value === currentAssigneeId);
-    const newAssigneeName = option?.label === 'Unassigned' ? null : option?.label;
-    setName(newAssigneeName ?? null);
-  }, [currentAssigneeId, options, setName]);
-};
-const filterOptions = (searchText: string, options: OptionType[]): OptionType[] => {
-  let newFilteredOptions = options;
-  if (searchText) {
-    newFilteredOptions = options.filter((option) =>
-      option.label.toLowerCase().includes(searchText.toLowerCase())
-    );
-  }
-  return newFilteredOptions;
-};
 
-export function Assignee(props: { form: FormInstance }) {
-  const { data: users } = useGetUsersQuery();
+export function Assignee() {
+  const [assignee, setAssignee] = useState('');
+  const options = useOptions();
 
-  const options: OptionType[] = useOptions(users);
-
-  const currentAssigneeId = props.form.getFieldValue('assignee');
-
-  const [name, setName] = useNameState(options, currentAssigneeId);
-  const [filteredOptions, setFilteredOptions] = useState<OptionType[]>(options);
-
-  useKeepFieldsInSync({ options, currentAssigneeId, setName });
-
-  const onSearch = (searchText: string): void => {
-    const newFilteredOptions = filterOptions(searchText, options);
-    setFilteredOptions(newFilteredOptions);
-  };
-  const onSelect = (assigneeId: string, option: OptionType): void => {
-    props.form.setFieldsValue({ assignee: assigneeId });
-    const newAssigneeName = option.label === 'Unassigned' ? null : option.label;
-    setName(newAssigneeName);
-  };
-  const onChange = (name: string) => {
-    setName(name);
+  const handleSelect = (value: string) => {
+    setAssignee(value);
   };
 
-  const validateStatus = (): '' | 'error' => {
-    if (!name) {
-      return '';
+  const filterOptions = (input: string, option: any) => {
+    if (!input || !option) {
+      return true;
     }
-    const currentAssignee = users?.find((user) => user._id === currentAssigneeId);
-    if (name === currentAssignee?.full_name) {
-      return '';
-    }
-    return 'error';
+    return String(option.children).toLowerCase().includes(input.toLowerCase());
   };
 
   return (
-    <>
-      <Form.Item label="Assignee" validateStatus={validateStatus()}>
-        <AutoComplete // element only displays full_name of selected assignee
-          placeholder="Unassigned"
-          value={name}
-          options={filteredOptions}
-          onSelect={onSelect}
-          onSearch={onSearch}
-          onChange={onChange}
-        />
-      </Form.Item>
-      <Form.Item // element that actually saves the assignee user id
-        hidden
-        label="assignee id - should be hidden"
-        name="assignee"
+    <Form.Item label="Assignee" name="assignee">
+      <Select
+        value={assignee}
+        onSelect={handleSelect}
+        showSearch
+        placeholder="Unassigned"
+        optionFilterProp="children"
+        filterOption={filterOptions}
       >
-        <Input />
-      </Form.Item>
-    </>
+        {options.map((option) => (
+          <Option key={option.value} value={option.value}>
+            {option.label}
+          </Option>
+        ))}
+      </Select>
+    </Form.Item>
   );
 }
