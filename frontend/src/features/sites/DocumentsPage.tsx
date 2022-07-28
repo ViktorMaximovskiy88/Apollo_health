a2b434694c098e5b80e0dfe212146dcd185fb62a
Ali Fardos <afardos@mmitnetwork.com>
2022-07-28 08:25:00 -0400

    Only showed Create Document for manual collection

--- frontend/src/features/sites/DocumentsPage.tsx
+++ frontend/src/features/sites/DocumentsPage.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from 'antd';
 import { MainLayout } from '../../components';
 import { DocumentsTable } from './DocumentsTable';
 import { AddDocumentModal } from "../collections/addDocumentModal";
-
+import { useGetSiteQuery } from './sitesApi';
 import { SiteMenu } from './SiteMenu';
+import { CollectionMethod } from "./types";
 
 export function DocumentsPage() {
   const params = useParams();
-  const [newDocumentModalVisible, setNewDocumentModalVisible]= useState(false);
+  const [newDocumentModalVisible, setNewDocumentModalVisible] = useState(false);
+  const { data: site } = useGetSiteQuery(params.siteId);
   return (
     <MainLayout
       sidebar={<SiteMenu />}
       pageTitle={'Documents'}
       pageToolbar={
-        <>
-          <Button onClick={() => setNewDocumentModalVisible(true)} className="ml-auto">Create Document</Button>
-        </>
+        site && site.collection_method == CollectionMethod.Manual ?
+        <Button onClick={() => setNewDocumentModalVisible(true)} className="ml-auto">Create Document</Button>
+        :
+        null
       }
     >
       <DocumentsTable />
