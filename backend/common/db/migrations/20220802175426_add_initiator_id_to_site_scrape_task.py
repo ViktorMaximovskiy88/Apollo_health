99ebe8a08149a9d231cfe3f60cf2a87d219f4f06
Jeremy Aldrich <jaldrich@mmitnetwork.com>
2022-08-03 11:15:02 -0400

    rename sitescrapetask user_id to initiator_id

--- backend/common/db/migrations/20220802175426_add_initiator_id_to_site_scrape_task.py
+++ backend/common/db/migrations/20220802175426_add_initiator_id_to_site_scrape_task.py
@@ -5,16 +5,16 @@ from backend.common.models.site_scrape_task import SiteScrapeTask
 
 class Forward:
     @iterative_migration()
-    async def add_user_id_to_site_scrape_task(
+    async def add_initiator_id_to_site_scrape_task(
         self, input_document: SiteScrapeTask, output_document: SiteScrapeTask
     ):
-        if not input_document.user_id:
-            output_document.user_id = None
+        if not input_document.initiator_id:
+            output_document.initiator_id = None
 
 
 class Backward:
     @iterative_migration()
-    async def remove_user_id_from_site_scrape_task(
+    async def remove_initiator_id_from_site_scrape_task(
         self, input_document: SiteScrapeTask, output_document: SiteScrapeTask
     ):
-        output_document.user_id = None
+        output_document.initiator_id = None
