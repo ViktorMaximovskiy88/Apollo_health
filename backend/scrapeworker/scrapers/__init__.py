0f47f3f068f3cf03c70163010b4f32f95c59ec56
Jeff Loiola <jloiola@mmitnetwork.com>
2022-08-03 15:02:25 -0400

    mv

--- backend/scrapeworker/scrapers/__init__.py
+++ backend/scrapeworker/scrapers/__init__.py
@@ -1,7 +1,7 @@
 from typing import Type
 
 from backend.scrapeworker.scrapers.aspnet_webform import AspNetWebFormScraper
-from backend.scrapeworker.scrapers.contentful_scraper import ContentfulScraper
+from backend.scrapeworker.scrapers.contentful import ContentfulScraper
 from backend.scrapeworker.scrapers.direct_download import (
     DirectDownloadScraper,
     PlaywrightBaseScraper,
