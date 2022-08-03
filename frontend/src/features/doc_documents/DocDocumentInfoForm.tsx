fdcaefa181c2cef069cb2c676e260ae6f797266b
Andrew Horn <ahorn@mmitnetwork.com>
2022-08-03 02:09:45 -0500

    added basic modal for add document family

--- frontend/src/features/doc_documents/DocDocumentInfoForm.tsx
+++ frontend/src/features/doc_documents/DocDocumentInfoForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, FormInstance, Select, Button } from 'antd';
+import { Form, Input, FormInstance, Select, Button, Modal } from 'antd';
 import { Hr } from '../../components';
 import { DocDocument } from './types';
 import { DateFields } from './DocDocumentDateFields';
@@ -6,6 +6,9 @@ import { DocumentClassification } from './DocDocumentClassificationFields';
 import { ExtractionFields } from './DocDocumentExtractionFields';
 import { UrlFields } from './DocDocumentUrlFields';
 import { PlusOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+const { Option } = Select;
 
 const Name = () => (
   <Form.Item name="name" label="Name" required={true}>
@@ -13,31 +16,51 @@ const Name = () => (
   </Form.Item>
 );
 
+interface ModalPropTypes {
+  onOk: () => void;
+  onCancel: () => void;
+}
+const useModal = (): [boolean, () => void, ModalPropTypes] => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showModal = () => {
+    setIsVisible(true);
+  };
+
+  const onOk = () => {
+    setIsVisible(false);
+  };
+
+  const onCancel = () => {
+    setIsVisible(false);
+  };
+
+  const modalProps = { onOk, onCancel };
+
+  return [isVisible, showModal, modalProps];
+};
+
 const DocumentFamily = () => {
-  const { Option } = Select;
   const children = [];
 
   for (let i = 10; i < 36; i++) {
-    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+    children.push({ key: i.toString(36) + i, label: i.toString(36) + i });
   }
+  const [options, setOptions] = useState(children);
 
-  const handleChange = (value: string[]) => {
-    console.log(`selected ${value}`);
-  };
+  const [isModalVisible, showModal, modalProps] = useModal();
+
   return (
     <div className="flex space-x-8">
+      <Modal {...modalProps} visible={isModalVisible} />
       <Form.Item name="document_family" label="Document Family" className="flex-1">
-        <Select
-          mode="multiple"
-          allowClear
-          placeholder="Please select"
-          defaultValue={['a10', 'c12']}
-          onChange={handleChange}
-        >
-          {children}
+        <Select mode="multiple" allowClear placeholder="None selected" defaultValue={[]}>
+          {options.map(({ key, label }) => (
+            <Option key={key}>{label}</Option>
+          ))}
         </Select>
       </Form.Item>
-      <Button className="flex-1 my-7" type="dashed">
+      <Button className="flex-1 my-7" type="dashed" onClick={showModal}>
         <PlusOutlined />
         Add New Document Family
       </Button>
