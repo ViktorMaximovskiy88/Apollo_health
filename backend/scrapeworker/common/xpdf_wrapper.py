ca8215149bb2747e466704ba8987b322834ea879
Jeff Loiola <jloiola@mmitnetwork.com>
2022-08-08 09:58:38 -0400

    latin characters need a diff text enc/dec specified

--- backend/scrapeworker/common/xpdf_wrapper.py
+++ backend/scrapeworker/common/xpdf_wrapper.py
@@ -1,5 +1,6 @@
 import asyncio
 
+
 async def pdfinfo(dest_path):
     process = await asyncio.create_subprocess_exec(
         "pdfinfo",
@@ -8,20 +9,21 @@ async def pdfinfo(dest_path):
         stderr=asyncio.subprocess.STDOUT,
     )
     pdfinfo_out, _ = await process.communicate()
-    info = pdfinfo_out.decode("utf-8", "ignore").strip()
+    info = pdfinfo_out.decode("iso-8859-1", "ignore").strip()
     metadata = {}
     key = ""
     for line in info.split("\n"):
         if not line.strip():
             continue
-        if not ":" in line: # assume single value is broken into multiple lines
+        if ":" not in line:  # assume single value is broken into multiple lines
             value = line.strip()
-            metadata[key] = f'{metadata[key]}; {value}'
+            metadata[key] = f"{metadata[key]}; {value}"
             continue
         key, value = line.split(":", 1)
         value = value.strip()
         metadata[key] = value
     return metadata
+
 
 async def pdftotext(dest_path):
     process = await asyncio.create_subprocess_exec(
@@ -32,4 +34,4 @@ async def pdftotext(dest_path):
         stderr=asyncio.subprocess.STDOUT,
     )
     pdftext_out, _ = await process.communicate()
-    return pdftext_out.decode("utf-8", "ignore").strip()
+    return pdftext_out.decode("iso-8859-1", "ignore").strip()
