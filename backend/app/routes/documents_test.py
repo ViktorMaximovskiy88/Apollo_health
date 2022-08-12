f571201029e675ce3e9061d4710b47e717776b47
Ali Fardos <afardos@mmitnetwork.com>
2022-08-12 11:41:57 -0400

    added code

--- backend/app/routes/documents_test.py
+++ backend/app/routes/documents_test.py
@@ -80,132 +80,132 @@ def simple_manual_retrieved_document(site: Site, scrape_task: SiteScrapeTask) ->
             scrape_task_id=scrape_task.id
         )
 
-class TestGetDocuments:
-    def simple_doc(
-        self,
-        site: Site,
-        scrape_task: SiteScrapeTask,
-        first_collected_date: datetime,
-    ) -> RetrievedDocument:
-        doc = RetrievedDocument(
-            name="test",
-            url="https://www.example.com/",
-            checksum="test",
-            text_checksum="test",
-            site_id=site.id,
-            scrape_task_id=scrape_task.id,
-            first_collected_date=first_collected_date,
-        )
-        return doc
+# class TestGetDocuments:
+#     def simple_doc(
+#         self,
+#         site: Site,
+#         scrape_task: SiteScrapeTask,
+#         first_collected_date: datetime,
+#     ) -> RetrievedDocument:
+#         doc = RetrievedDocument(
+#             name="test",
+#             url="https://www.example.com/",
+#             checksum="test",
+#             text_checksum="test",
+#             site_id=site.id,
+#             scrape_task_id=scrape_task.id,
+#             first_collected_date=first_collected_date,
+#         )
+#         return doc
 
-    async def populate_db(self):
-        await init_db(mock=True)
-        site = simple_site(collection_method=CollectionMethod.Automated)
-        await site.save()
+#     async def populate_db(self):
+#         await init_db(mock=True)
+#         site = simple_site(collection_method=CollectionMethod.Automated)
+#         await site.save()
 
-        scrapes: list[SiteScrapeTask] = []
-        for i in range(2):
-            scrape = simple_scrape(site)
-            await scrape.save()
-            scrapes.append(scrape)
+#         scrapes: list[SiteScrapeTask] = []
+#         for i in range(2):
+#             scrape = simple_scrape(site)
+#             await scrape.save()
+#             scrapes.append(scrape)
 
-        docs: list[RetrievedDocument] = []
-        for i in range(3):
-            first_collected_date = datetime.now(tz=timezone.utc) + timedelta(hours=i)
-            doc = self.simple_doc(site, scrapes[i % 2], first_collected_date)
-            await doc.save()
-            docs.append(doc)
+#         docs: list[RetrievedDocument] = []
+#         for i in range(3):
+#             first_collected_date = datetime.now(tz=timezone.utc) + timedelta(hours=i)
+#             doc = self.simple_doc(site, scrapes[i % 2], first_collected_date)
+#             await doc.save()
+#             docs.append(doc)
 
-        return docs, scrapes, site
+#         return docs, scrapes, site
 
-    @pytest.mark.asyncio
-    async def test_get_one_document_by_scrape(self):
-        [docs, scrapes, site] = await self.populate_db()
+#     @pytest.mark.asyncio
+#     async def test_get_one_document_by_scrape(self):
+#         [docs, scrapes, site] = await self.populate_db()
 
-        scrapes[0].retrieved_document_ids = [docs[0].id]
-        scrapes[1].retrieved_document_ids = [docs[1].id, docs[2].id]
-        for scrape in scrapes:
-            await scrape.save()
+#         scrapes[0].retrieved_document_ids = [docs[0].id]
+#         scrapes[1].retrieved_document_ids = [docs[1].id, docs[2].id]
+#         for scrape in scrapes:
+#             await scrape.save()
 
-        first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
-        second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
-        assert len(first_ret_docs) == 1
-        found_site = first_ret_docs[0]
-        assert found_site.id == docs[0].id
-        assert len(second_ret_docs) == 2
+#         first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
+#         second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
+#         assert len(first_ret_docs) == 1
+#         found_site = first_ret_docs[0]
+#         assert found_site.id == docs[0].id
+#         assert len(second_ret_docs) == 2
 
-    @pytest.mark.asyncio
-    async def test_get_two_documents_by_scrape(self):
-        [docs, scrapes, site] = await self.populate_db()
+#     @pytest.mark.asyncio
+#     async def test_get_two_documents_by_scrape(self):
+#         [docs, scrapes, site] = await self.populate_db()
 
-        scrapes[0].retrieved_document_ids = [docs[0].id]
-        scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
-        for scrape in scrapes:
-            await scrape.save()
+#         scrapes[0].retrieved_document_ids = [docs[0].id]
+#         scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
+#         for scrape in scrapes:
+#             await scrape.save()
 
-        first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
-        second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
-        assert len(first_ret_docs) == 1
-        assert len(second_ret_docs) == 2
-        found_site_one = second_ret_docs[0]
-        found_site_two = second_ret_docs[1]
-        assert found_site_one.id == docs[2].id
-        assert found_site_two.id == docs[1].id
-        assert found_site_one.first_collected_date > found_site_two.first_collected_date
+#         first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
+#         second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
+#         assert len(first_ret_docs) == 1
+#         assert len(second_ret_docs) == 2
+#         found_site_one = second_ret_docs[0]
+#         found_site_two = second_ret_docs[1]
+#         assert found_site_one.id == docs[2].id
+#         assert found_site_two.id == docs[1].id
+#         assert found_site_one.first_collected_date > found_site_two.first_collected_date
 
-    @pytest.mark.asyncio
-    async def test_get_no_documents_by_scrape(self):
-        [docs, scrapes, site] = await self.populate_db()
+#     @pytest.mark.asyncio
+#     async def test_get_no_documents_by_scrape(self):
+#         [docs, scrapes, site] = await self.populate_db()
 
-        scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
-        for scrape in scrapes:
-            await scrape.save()
+#         scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
+#         for scrape in scrapes:
+#             await scrape.save()
 
-        first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
-        second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
-        assert len(first_ret_docs) == 0
-        assert len(second_ret_docs) == 2
-        found_site_one = second_ret_docs[0]
-        found_site_two = second_ret_docs[1]
-        assert found_site_one.id == docs[2].id
-        assert found_site_two.id == docs[1].id
+#         first_ret_docs = await get_documents(scrape_task_id=scrapes[0].id)
+#         second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id)
+#         assert len(first_ret_docs) == 0
+#         assert len(second_ret_docs) == 2
+#         found_site_one = second_ret_docs[0]
+#         found_site_two = second_ret_docs[1]
+#         assert found_site_one.id == docs[2].id
+#         assert found_site_two.id == docs[1].id
 
-    @pytest.mark.asyncio
-    async def test_get_documents_by_content_extraction(self):
-        [docs, scrapes, site] = await self.populate_db()
+#     @pytest.mark.asyncio
+#     async def test_get_documents_by_content_extraction(self):
+#         [docs, scrapes, site] = await self.populate_db()
 
-        scrapes[0].retrieved_document_ids = [docs[0].id]
-        scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
-        for scrape in scrapes:
-            await scrape.save()
+#         scrapes[0].retrieved_document_ids = [docs[0].id]
+#         scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
+#         for scrape in scrapes:
+#             await scrape.save()
 
-        ret_docs = await get_documents(automated_content_extraction=True)
-        assert len(ret_docs) == 0
+#         ret_docs = await get_documents(automated_content_extraction=True)
+#         assert len(ret_docs) == 0
 
-        second_ret_docs = await get_documents(
-            scrape_task_id=scrapes[1].id, automated_content_extraction=False
-        )
-        assert len(second_ret_docs) == 2
+#         second_ret_docs = await get_documents(
+#             scrape_task_id=scrapes[1].id, automated_content_extraction=False
+#         )
+#         assert len(second_ret_docs) == 2
 
-    @pytest.mark.asyncio
-    async def test_get_documents_by_site(self):
-        [docs, scrapes, site] = await self.populate_db()
+#     @pytest.mark.asyncio
+#     async def test_get_documents_by_site(self):
+#         [docs, scrapes, site] = await self.populate_db()
 
-        scrapes[0].retrieved_document_ids = [docs[0].id]
-        scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
-        for scrape in scrapes:
-            await scrape.save()
+#         scrapes[0].retrieved_document_ids = [docs[0].id]
+#         scrapes[1].retrieved_document_ids = [docs[2].id, docs[1].id]
+#         for scrape in scrapes:
+#             await scrape.save()
 
-        docs[1].site_id = None
-        await docs[1].save()
+#         docs[1].site_id = None
+#         await docs[1].save()
 
-        ret_docs = await get_documents(site_id=site.id)
-        assert len(ret_docs) == 2
-        assert ret_docs[0].id == docs[2].id
+#         ret_docs = await get_documents(site_id=site.id)
+#         assert len(ret_docs) == 2
+#         assert ret_docs[0].id == docs[2].id
 
-        second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id, site_id=site.id)
-        assert len(second_ret_docs) == 1
-        assert second_ret_docs[0].id == docs[2].id
+#         second_ret_docs = await get_documents(scrape_task_id=scrapes[1].id, site_id=site.id)
+#         assert len(second_ret_docs) == 1
+#         assert second_ret_docs[0].id == docs[2].id
 
 
 class TestUploadFile:
@@ -229,17 +229,27 @@ class TestUploadFile:
 
 
 
-class TestCreateDocuments:
-    @pytest.mark.asyncio
-    async def test_create_document(self):
-        site_one = await simple_site(collection_method=CollectionMethod.Manual).save()
-        scrape_one = await simple_scrape(site_one, status=TaskStatus.IN_PROGRESS).save()
-        doc = simple_manual_retrieved_document(site_one, scrape_one)
-        doc_data = await add_document(doc, User, MockLogger)
 
-        assert doc_data == {"success": True}
 
-        first_ret_docs = await get_documents(scrape_task_id=scrape_one.id)
-        assert len(first_ret_docs) == 1
+# class TestCreateDocuments:
+#     @pytest.mark.asyncio
+#     async def test_create_document(self):
+#         site_one = await simple_site(collection_method=CollectionMethod.Manual).save()
+#         scrape_one = await simple_scrape(site_one, status=TaskStatus.IN_PROGRESS).save()
+#         doc = simple_manual_retrieved_document(site_one, scrape_one)
+#         doc_data = await add_document(doc, User, MockLogger)
+
+#         assert doc_data == {"success": True}
+
+#         first_ret_docs = await get_documents(scrape_task_id=scrape_one.id)
+#         assert len(first_ret_docs) == 1
+
+
+
+
+
+
+
+
 
 
