89661761a05b99e92dc5932bf6761a8d729d2aa2
Jeff Loiola <jloiola@mmitnetwork.com>
2022-08-15 12:46:31 -0400

    fixed mocks; lints

--- backend/app/routes/documents_test.py
+++ backend/app/routes/documents_test.py
@@ -1,38 +1,51 @@
+import tempfile
 from datetime import datetime, timedelta, timezone
 from random import random
 
+import aiofiles
 import pytest
 import pytest_asyncio
-import aiofiles
-from pydantic import HttpUrl
-from beanie import Document, PydanticObjectId
-from fastapi import UploadFile
 import requests
-import tempfile
+from beanie import Document
+from fastapi import UploadFile
+from pydantic import HttpUrl
 
+from backend.app.routes.documents import add_document, get_documents, upload_document
 from backend.common.core.enums import CollectionMethod, SiteStatus, TaskStatus
 from backend.common.db.init import init_db
 from backend.common.models.document import RetrievedDocument, RetrievedDocumentLimitTags
 from backend.common.models.site import BaseUrl, ScrapeMethodConfiguration, Site
 from backend.common.models.site_scrape_task import SiteScrapeTask
 from backend.common.models.user import User
-from backend.app.routes.documents import (
-    get_documents,
-    add_document,
-    upload_document
-)
 
 RetrievedDocumentLimitTags.Settings.projection = None  # type: ignore
+
 
 @pytest_asyncio.fixture(autouse=True)
 async def before_each_test():
     random_name = str(random())
     await init_db(mock=True, database_name=random_name)
 
+
+@pytest_asyncio.fixture()
+@pytest.mark.asyncio()
+async def user():
+    user = User(
+        id="62e7c6647e2a94f469d57f34",
+        email="example@me.com",
+        full_name="John Doe",
+        hashed_password="example",
+    )
+    await user.save()
+
+    return user
+
+
 class MockLogger:
     async def background_log_change(current_user: User, site_scrape_task: Document, action: str):
         assert type(action) == str
         return None
+
 
 def simple_scrape(site: Site, status=TaskStatus.QUEUED) -> SiteScrapeTask:
     return SiteScrapeTask(
@@ -40,6 +53,7 @@ def simple_scrape(site: Site, status=TaskStatus.QUEUED) -> SiteScrapeTask:
         status=status,
         queued_time=datetime.now(tz=timezone.utc),
     )
+
 
 def simple_site(
     disabled=False,
@@ -67,19 +81,19 @@ def simple_site(
         base_urls=base_urls,
     )
 
-def simple_manual_retrieved_document(
-    site: Site, 
-    scrape_task: SiteScrapeTask,
-    checksum = "test",
-    text_checksum = "test",
-    content_type = None,
-    file_extension = None,
-    metadata = {},
-    doc_type_confidence = None,
-    therapy_tags = [],
-    indication_tags = [],
-    identified_dates = []
 
+def simple_manual_retrieved_document(
+    site: Site,
+    scrape_task: SiteScrapeTask,
+    checksum="test",
+    text_checksum="test",
+    content_type=None,
+    file_extension=None,
+    metadata={},
+    doc_type_confidence=None,
+    therapy_tags=[],
+    indication_tags=[],
+    identified_dates=[],
 ) -> RetrievedDocument:
     return RetrievedDocument(
         name="test",
@@ -96,8 +110,9 @@ def simple_manual_retrieved_document(
         doc_type_confidence=doc_type_confidence,
         therapy_tags=therapy_tags,
         indication_tags=indication_tags,
-        identified_dates=identified_dates
+        identified_dates=identified_dates,
     )
+
 
 class TestGetDocuments:
     def simple_doc(
@@ -229,89 +244,70 @@ class TestGetDocuments:
 
 class TestUploadFile:
     @pytest.mark.asyncio
-    async def test_upload_file(self):
+    async def test_upload_file(self, user):
         URL = "https://parprdusemmitst01.blob.core.windows.net/autohunteddocs/7c8418d4-054b-4fa4-9b97-d3f75c353dd1/7c8418d4-054b-4fa4-9b97-d3f75c353dd1.pdf"  # noqa
         response = requests.get(URL)
         with tempfile.NamedTemporaryFile() as temp:
             async with aiofiles.open(temp.name, "wb") as fd:
                 await fd.write(response.content)
-                
-                upload_file = UploadFile(filename="test.pdf", file=temp, content_type="application/pdf")
-                uploaded_document = await upload_document(upload_file, User, MockLogger)
 
-                assert uploaded_document['success'] == True
-                assert uploaded_document['data']['checksum'] != None
-                assert uploaded_document['data']['text_checksum'] != None
-                assert uploaded_document['data']['metadata'] != None
-                assert uploaded_document['data']['doc_type_confidence'] != None
-    
+                upload_file = UploadFile(
+                    filename="test.pdf", file=temp, content_type="application/pdf"
+                )
+                uploaded_document = await upload_document(upload_file, user, MockLogger)
+
+                assert uploaded_document["success"] is True
+                assert uploaded_document["data"]["checksum"] is not None
+                assert uploaded_document["data"]["text_checksum"] is not None
+                assert uploaded_document["data"]["metadata"] is not None
+                assert uploaded_document["data"]["doc_type_confidence"] is not None
 
     @pytest.mark.asyncio
-    async def test_upload_create_document(self):
+    async def test_upload_create_document(self, user):
         URL = "https://parprdusemmitst01.blob.core.windows.net/autohunteddocs/7c8418d4-054b-4fa4-9b97-d3f75c353dd1/7c8418d4-054b-4fa4-9b97-d3f75c353dd1.pdf"  # noqa
         response = requests.get(URL)
         with tempfile.NamedTemporaryFile() as temp:
             async with aiofiles.open(temp.name, "wb") as fd:
                 await fd.write(response.content)
 
-                upload_file = UploadFile(filename="test.pdf", file=temp, content_type="application/pdf")
-                uploaded_document = await upload_document(upload_file, User, MockLogger)
-                
-                assert uploaded_document['success'] == True
+                upload_file = UploadFile(
+                    filename="test.pdf", file=temp, content_type="application/pdf"
+                )
+                uploaded_document = await upload_document(upload_file, user, MockLogger)
+
+                assert uploaded_document["success"] is True
                 site_one = await simple_site(collection_method=CollectionMethod.Manual).save()
                 scrape_one = await simple_scrape(site_one, status=TaskStatus.IN_PROGRESS).save()
                 doc = simple_manual_retrieved_document(
-                    site_one, 
+                    site_one,
                     scrape_one,
-                    checksum=uploaded_document['data']['checksum'],
-                    text_checksum=uploaded_document['data']['text_checksum'],
-                    content_type=uploaded_document['data']['content_type'],
-                    file_extension=uploaded_document['data']['file_extension'],
-                    metadata=uploaded_document['data']['metadata'],
-                    doc_type_confidence=uploaded_document['data']['doc_type_confidence'],
-                    therapy_tags=uploaded_document['data']['therapy_tags'],
-                    indication_tags=uploaded_document['data']['indication_tags'],
-                    identified_dates=uploaded_document['data']['identified_dates'],
+                    checksum=uploaded_document["data"]["checksum"],
+                    text_checksum=uploaded_document["data"]["text_checksum"],
+                    content_type=uploaded_document["data"]["content_type"],
+                    file_extension=uploaded_document["data"]["file_extension"],
+                    metadata=uploaded_document["data"]["metadata"],
+                    doc_type_confidence=uploaded_document["data"]["doc_type_confidence"],
+                    therapy_tags=uploaded_document["data"]["therapy_tags"],
+                    indication_tags=uploaded_document["data"]["indication_tags"],
+                    identified_dates=uploaded_document["data"]["identified_dates"],
                 )
-                user = User(
-                    email="example@me.com",
-                    full_name="John Doe",
-                    hashed_password="example"
-                )
-                await user.save()
-                
+
                 doc_data = await add_document(doc, user, MockLogger)
-                assert doc_data['success'] == True
-                uploaded_document_2 = await upload_document(upload_file, User, MockLogger)
+                assert doc_data["success"] is True
+                uploaded_document_2 = await upload_document(upload_file, user, MockLogger)
 
-                assert uploaded_document_2['error'] == "The document already exists!"
-
+                assert uploaded_document_2["error"] == "The document already exists!"
 
 
 class TestCreateDocuments:
     @pytest.mark.asyncio
-    async def test_create_document(self):
+    async def test_create_document(self, user):
         site_one = await simple_site(collection_method=CollectionMethod.Manual).save()
         scrape_one = await simple_scrape(site_one, status=TaskStatus.IN_PROGRESS).save()
         doc = simple_manual_retrieved_document(site_one, scrape_one)
-        
-        user = User(
-            email="example@me.com",
-            full_name="John Doe",
-            hashed_password="example"
-        )
-        await user.save()
 
         doc_data = await add_document(doc, user, MockLogger)
         assert doc_data == {"success": True}
 
         first_ret_docs = await get_documents(scrape_task_id=scrape_one.id)
         assert len(first_ret_docs) == 1
-
-
-
-
-
-
-
-
