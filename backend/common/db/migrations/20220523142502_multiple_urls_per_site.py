a68c9d3e9985581d9d1fb886a83c3d0ee0d39452
Jeff Loiola <jloiola@mmitnetwork.com>
2022-08-23 18:46:54 -0400

    rm the correct migration even

---
+++ backend/common/db/migrations/20220523142502_multiple_urls_per_site.py
@@ -0,0 +1,17 @@
+from beanie import iterative_migration
+
+from backend.common.models.site import BaseUrl, NoScrapeConfigSite, SingleUrlSite
+
+
+class Forward:
+    @iterative_migration()
+    async def url_to_urls(self, input_document: SingleUrlSite, output_document: NoScrapeConfigSite):
+        if input_document.base_url is None:
+            return
+        output_document.base_urls = [BaseUrl(url=input_document.base_url)]
+
+
+class Backward:
+    @iterative_migration()
+    async def urls_to_url(self, input_document: NoScrapeConfigSite, output_document: SingleUrlSite):
+        output_document.base_url = input_document.base_urls[0].url
