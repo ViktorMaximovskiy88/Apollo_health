ea434237b477ca73754b731f36ac24114405de9f
Jeff Loiola <jloiola@mmitnetwork.com>
2022-08-24 14:20:59 -0400

    migrate

--- backend/common/db/migrations/20220823184707_locations.py
+++ backend/common/db/migrations/20220823184707_locations.py
@@ -9,20 +9,63 @@ from backend.common.models.document import RetrievedDocument
 class Forward:
     @free_fall_migration(document_models=[DocDocument, RetrievedDocument])
     async def add_locations(self, session):
-        pass
-        # @above
-        # base_url
-        # url
-        # link_text
-        # closest_header
-        # site_id
-        # context_metadata
-        # first_collected_date
-        # last_collected_date
 
-        # for every rt doc
-        # move all above to location
-        # unset site_id, url, base_url, scrape_task_id, logical_document_id, logical_document_version, context_metadata
+        # choosing to nuke any doc thats duped (20)
+        # can recover via normal scrape
+        checksums = []
+        async for doc in RetrievedDocument.get_motor_collection().aggregate(
+            [
+                {"$group": {"_id": "$checksum", "count": {"$sum": 1}}},
+                {"$match": {"count": {"$gt": 1}}},
+            ]
+        ):
+            checksums.append(doc["_id"])
+
+        result = await RetrievedDocument.get_motor_collection().delete_many(
+            {"checksum": {"$in": checksums}}
+        )
+
+        logging.info(
+            f"removing dupe RetrievedDocument checksums -> acknowledged={result.acknowledged} deleted_count={result.deleted_count} checksums={len(checksums)}"  # noqa
+        )
+
+        result = await RetrievedDocument.get_motor_collection().update_many(
+            {},
+            [
+                {
+                    "$set": {
+                        "locations": [
+                            {
+                                "base_url": "$base_url",
+                                "url": "$url",
+                                "site_id": "$site_id",
+                                "first_collected_date": "$first_collected_date",
+                                "last_collected_date": "$last_collected_date",
+                                "context_metadata": "$context_metadata",
+                                "link_text": "$context_metadata.link_text",
+                                "closest_heading": "$context_metadata.closest_heading",
+                            }
+                        ]
+                    },
+                },
+                {
+                    "$unset": [
+                        "site_id",
+                        "url",
+                        "base_url",
+                        "scrape_task_id",
+                        "logical_document_id",
+                        "logical_document_version",
+                        "context_metadata",
+                        "file_checksum_aliases",
+                    ]
+                },
+            ],
+        )
+
+        logging.info(
+            f"updating RetrievedDocument locations -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+        )
 
         # site_id
         # base_url
@@ -44,6 +87,48 @@ class Forward:
 
         # where else might doc docs or rt docs refs live?
         # remove all change logs related to the loser rt and doc docs
+
+        checksums = []
+        async for doc in DocDocument.get_motor_collection().aggregate(
+            [
+                {"$group": {"_id": "$checksum", "count": {"$sum": 1}}},
+                {"$match": {"count": {"$gt": 1}}},
+            ]
+        ):
+            checksums.append(doc["_id"])
+
+        result = await DocDocument.get_motor_collection().delete_many(
+            {"checksum": {"$in": checksums}}
+        )
+
+        logging.info(
+            f"removing dupe DocDocument checksums -> acknowledged={result.acknowledged} deleted_count={result.deleted_count} checksums={len(checksums)}"  # noqa
+        )
+
+        result = await DocDocument.get_motor_collection().update_many(
+            {},
+            [
+                {
+                    "$set": {
+                        "locations": [
+                            {
+                                "base_url": "$base_url",
+                                "url": "$url",
+                                "site_id": "$site_id",
+                                "link_text": "$context_metadata.link_text",
+                                "first_collected_date": "$first_collected_date",
+                                "last_collected_date": "$last_collected_date",
+                            }
+                        ]
+                    },
+                },
+                {"$unset": ["site_id", "url", "base_url", "link_text"]},
+            ],
+        )
+
+        logging.info(
+            f"updating DocDocument locations -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+        )
 
 
 class Backward:
