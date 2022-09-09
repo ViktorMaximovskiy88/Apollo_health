abbcd120719afd914f95d05fc1f31633eb0ffa18
James Thomas <jthomas@mmitnetwork.com>
2022-09-09 10:52:32 -0400

    remove unusued siteScrapeTasks endpoint call

--- frontend/src/features/collections/addDocumentModal.tsx
+++ frontend/src/features/collections/addDocumentModal.tsx
@@ -23,10 +23,9 @@ import {
 
 import { prettyDate } from '../../common';
 import { useAddDocumentMutation } from '../retrieved_documents/documentsApi';
-import { useGetScrapeTasksForSiteQuery } from './siteScrapeTasksApi';
-import { baseApiUrl, client, fetchWithAuth } from '../../app/base-api';
-import { RetrievedDocument, DocumentTypes, LanguageCodes } from '../retrieved_documents/types';
-import { DocDocument } from '../doc_documents/types';
+import { baseApiUrl, client } from '../../app/base-api';
+import { DocumentTypes, languageCodes } from '../retrieved_documents/types';
+import { SiteDocDocument } from '../doc_documents/types';
 
 interface AddDocumentModalPropTypes {
   oldVersion?: DocDocument;
@@ -51,18 +50,22 @@ export function AddDocumentModal({ oldVersion, setVisible, siteId }: AddDocument
       base_url: oldVersion.base_url,
     };
   }
+  /* eslint-disable no-template-curly-in-string */
   const validateMessages = {
     required: '${label} is required!',
     types: {
       url: '${label} is not a valid url!',
     },
   };
+  /* eslint-enable no-template-curly-in-string */
+
   async function saveDocument(newDocument: any) {
     try {
       newDocument.site_id = siteId;
-      if (scrapeTasks) {
-        newDocument.scrape_task_id = scrapeTasks[0]._id;
-      }
+      //  we nuked this relationship
+      // if (scrapeTasks) {
+      //   newDocument.scrape_task_id = scrapeTasks[0]._id;
+      // }
       // used to determine how we handle this request if new_version or new document
       if (oldVersion) {
         newDocument._id = oldVersion._id;
@@ -220,7 +223,7 @@ function DocumentTypeItem() {
 function LanguageItem() {
   return (
     <Form.Item className="grow" name="lang_code" label="Language" rules={[{ required: true }]}>
-      <Select options={LanguageCodes} />
+      <Select options={languageCodes} />
     </Form.Item>
   );
 }
