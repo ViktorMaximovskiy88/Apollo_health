5e311a4c3739270cece6501ca980546bd15a5ab4
Liam Tiernan <ltiernan@mmitnetwork.com>
2022-09-19 09:49:23 -0400

    Working commit - handle search navigations

--- backend/scrapeworker/search_crawler.py
+++ backend/scrapeworker/search_crawler.py
@@ -1,20 +1,11 @@
-from dataclasses import dataclass
 from logging import Logger
 
-from playwright.async_api import Page
+from playwright.async_api import Page, TimeoutError
 
 from backend.common.models.search_codes import SearchCodeSet
 from backend.common.models.site import ScrapeMethodConfiguration
 from backend.scrapeworker.common.selectors import to_xpath
 from backend.scrapeworker.playbook import PlaybookContext, ScrapePlaybook
-
-
-@dataclass
-class NavigationState:
-    has_navigated: bool
-
-    def handle_nav(self):
-        self.has_navigated = True
 
 
 class SearchableCrawler:
@@ -62,34 +53,33 @@ class SearchableCrawler:
         except Exception:
             return
 
-    async def __search(self, page: Page, nav_state: NavigationState):
+    async def __search(self, page: Page):
         try:
-            # TODO: figure this out. Not returning event?
-            async with page.expect_event("load") as event:
-                if self.submit_selector:
-                    await page.click(self.submit_selector)
-                else:
-                    await page.keyboard.press("Enter")
-            await event.value
-            nav_state.handle_nav()
-        except Exception:
-            # some additional handling here
-            return
+            async with page.expect_event("load", timeout=10000):
+                try:
+                    if self.submit_selector:
+                        await page.click(self.submit_selector, timeout=1)
+                    else:
+                        await page.keyboard.press("Enter")
+                except TimeoutError as err:
+                    raise err
+            return True
+        except TimeoutError:
+            return False
 
     async def run_searchable(self, page: Page, playbook_context: PlaybookContext):
         base_url = page.url
         codes = await self.__codes()
-        nav_state = NavigationState(has_navigated=False)
 
         for code in codes:
-            nav_state.has_navigated = False
+            has_navigated = False
             try:
                 await self.__type(page, code)
                 await self.__select(page, code)
-                await self.__search(page, nav_state)
+                has_navigated = await self.__search(page)
                 yield code
             except Exception:
                 self.log.error("Searchable Execution Error", exc_info=True)
-            if nav_state.has_navigated:
+            if has_navigated:
                 await page.goto(base_url)
                 await self.replay_playbook(page, playbook_context)
