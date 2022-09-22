8b1cdafc7277e4028dcf98a34890161ba83ed889
Jeff Loiola <jloiola@mmitnetwork.com>
2022-09-22 13:00:37 -0400

    yolo

--- backend/scrapeworker/scrape_worker.py
+++ backend/scrapeworker/scrape_worker.py
@@ -472,7 +472,6 @@ class ScrapeWorker:
         await self.wait_for_completion_or_cancel(tasks)
 
         doc_ids = [doc.id for (doc, doc_doc) in self.lineage_tasks]
-        # if False:
         await self.lineage_service.process_lineage_for_doc_ids(self.site.id, doc_ids)
 
         await self.downloader.close()
