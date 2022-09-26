a2840775f7b7532913a5ec0b7e30f03c672e6758
James Thomas <jthomas@mmitnetwork.com>
2022-09-26 13:27:04 -0400

    Rename DocumentFamilyHomePage

--- frontend/src/features/doc_documents/document_family/DocumentFamilyPage.tsx
+++
@@ -1,6 +0,0 @@
-import { MainLayout } from '../../../components';
-import { SiteMenu } from '../../sites/SiteMenu';
-
-export function DocumentFamilyPage() {
-  return <MainLayout sidebar={<SiteMenu />} pageTitle={'Document Family'}></MainLayout>;
-}
