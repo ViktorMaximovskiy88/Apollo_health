769d25694c70a7a60374f68f70d8d02cc7fd2583
Jeff Loiola <jloiola@mmitnetwork.com>
2022-09-27 14:01:09 -0400

    fix path

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
+from pathlib import Path
+
 import newrelic.agent
 
-newrelic.agent.initialize("./backend/app/newrelic.ini")
+newrelic.agent.initialize(Path(__file__).parent / "newrelic.ini")
 
 import sys
-from pathlib import Path
 
 import uvicorn
 
 if __name__ == "__main__":
+
     sys.path.append(str(Path(__file__).parent.joinpath("../..").resolve()))
     from backend.common.core.config import is_local
 
