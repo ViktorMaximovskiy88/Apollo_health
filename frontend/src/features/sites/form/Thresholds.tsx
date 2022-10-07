1f6e6d34aea2526febd8c9e8e3aece772ecf48a3
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-07 12:45:06 -0500

    fixed bug where tooltip would not follow element on scroll

--- frontend/src/features/sites/form/Thresholds.tsx
+++ frontend/src/features/sites/form/Thresholds.tsx
@@ -9,7 +9,16 @@ const formatter = (value?: number) => {
 export function DocumentTypeThreshold() {
   return (
     <Form.Item name="doc_type_threshold" label="Document Type Threshold">
-      <Slider min={0} max={1} step={0.01} tipFormatter={formatter} tooltip={{ open: true }} />
+      <Slider
+        min={0}
+        max={1}
+        step={0.01}
+        tipFormatter={formatter}
+        tooltip={{
+          open: true,
+          getPopupContainer: (triggerNode) => triggerNode.parentElement ?? triggerNode, // fixes tooltip to be inline with element -- https://github.com/ant-design/ant-design/issues/25117#issuecomment-873747921
+        }}
+      />
     </Form.Item>
   );
 }
@@ -17,7 +26,16 @@ export function DocumentTypeThreshold() {
 export function LineageThreshold() {
   return (
     <Form.Item name="lineage_threshold" label="Lineage Threshold">
-      <Slider min={0} max={1} step={0.01} tipFormatter={formatter} tooltip={{ open: true }} />
+      <Slider
+        min={0}
+        max={1}
+        step={0.01}
+        tipFormatter={formatter}
+        tooltip={{
+          open: true,
+          getPopupContainer: (triggerNode) => triggerNode.parentElement ?? triggerNode, // fixes tooltip to be inline with element -- https://github.com/ant-design/ant-design/issues/25117#issuecomment-873747921
+        }}
+      />
     </Form.Item>
   );
 }
