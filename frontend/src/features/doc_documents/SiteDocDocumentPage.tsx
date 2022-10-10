17a04c582d59b90818bd2f6f3ac2f405ff4485c0
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-10 12:39:53 -0500

    resolved visible->open deprecation warnings from antd

--- frontend/src/features/doc_documents/SiteDocDocumentPage.tsx
+++ frontend/src/features/doc_documents/SiteDocDocumentPage.tsx
@@ -10,17 +10,17 @@ import { AddDocumentModal } from '../collections/addDocumentModal';
 import { SiteDocDocument } from './types';
 
 export function SiteDocDocumentsPage() {
-  const [newDocumentModalVisible, setNewDocumentModalVisible] = useState(false);
+  const [newDocumentModalOpen, setNewDocumentModalOpen] = useState(false);
   const [oldVersion, setOldVersion] = useState<any>();
   const { siteId } = useParams();
 
   function handleNewVersion(data: SiteDocDocument) {
     setOldVersion(data);
-    setNewDocumentModalVisible(true);
+    setNewDocumentModalOpen(true);
   }
 
   function hideNewDocument(type: boolean) {
-    setNewDocumentModalVisible(false);
+    setNewDocumentModalOpen(false);
     setOldVersion(null);
   }
 
@@ -29,14 +29,14 @@ export function SiteDocDocumentsPage() {
       sidebar={<SiteMenu />}
       sectionToolbar={
         <>
-          <Button onClick={() => setNewDocumentModalVisible(true)} className="ml-auto">
+          <Button onClick={() => setNewDocumentModalOpen(true)} className="ml-auto">
             Create Document
           </Button>
         </>
       }
     >
-      {newDocumentModalVisible && (
-        <AddDocumentModal oldVersion={oldVersion} setVisible={hideNewDocument} siteId={siteId} />
+      {newDocumentModalOpen && (
+        <AddDocumentModal oldVersion={oldVersion} setOpen={hideNewDocument} siteId={siteId} />
       )}
       <SiteDocDocumentsTable handleNewVersion={handleNewVersion} />
     </MainLayout>
--- frontend/src/features/sites/form/ThresholdWithOverride.tsx
+++ frontend/src/features/sites/form/ThresholdWithOverride.tsx
@@ -28,14 +28,13 @@ export function ThresholdWithOverride({
       {thresholdOverride ? (
         <Form.Item name={thresholdName} label={thresholdLabel}>
           <Slider
-            defaultValue={0.75}
             min={0}
             max={1}
             step={0.01}
-            tipFormatter={formatter}
             tooltip={{
               open: true,
               placement: 'left',
+              formatter,
               getPopupContainer: (triggerNode) => triggerNode.parentElement ?? triggerNode, // fixes tooltip to be inline with element -- https://github.com/ant-design/ant-design/issues/25117#issuecomment-873747921
             }}
           />
