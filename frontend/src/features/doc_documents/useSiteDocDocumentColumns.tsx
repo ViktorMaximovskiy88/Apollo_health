626e7014eeb9c8159e6ef9f750431c8d5a685712
Niven, Kaleb <kniven@mmitnetwork.com>
2022-10-11 13:28:42 +0000

    Merged PR 8523: APH-536: use UTC date without converting to local in doc document data table
    APH-536: use UTC date without converting to local in doc document data table

--- frontend/src/features/doc_documents/useSiteDocDocumentColumns.tsx
+++ frontend/src/features/doc_documents/useSiteDocDocumentColumns.tsx
@@ -4,7 +4,7 @@ import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter';
 import BoolFilter from '@inovua/reactdatagrid-community/BoolFilter';
 import { Button } from 'antd';
 import { LinkOutlined, CheckCircleFilled } from '@ant-design/icons';
-import { prettyDateFromISO, prettyDateTimeFromISO } from '../../common';
+import { prettyDateUTCFromISO } from '../../common';
 import { SiteDocDocument } from './types';
 import { Link } from 'react-router-dom';
 import { DocumentTypes } from '../retrieved_documents/types';
@@ -42,7 +42,7 @@ export const createColumns = ({ handleNewVersion }: CreateColumnsType) => {
       },
       render: ({ data: doc }: { data: SiteDocDocument }) => {
         if (!doc.last_collected_date) return null;
-        return prettyDateTimeFromISO(doc.last_collected_date);
+        return prettyDateUTCFromISO(doc.last_collected_date);
       },
     },
     {
@@ -102,7 +102,7 @@ export const createColumns = ({ handleNewVersion }: CreateColumnsType) => {
       },
       render: ({ data: doc }: { data: SiteDocDocument }) => {
         if (doc.final_effective_date) {
-          return prettyDateFromISO(doc.final_effective_date);
+          return prettyDateUTCFromISO(doc.final_effective_date);
         }
       },
     },
