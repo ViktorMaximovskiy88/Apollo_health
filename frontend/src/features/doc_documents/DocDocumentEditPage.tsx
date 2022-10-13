daee4e967fb0d3419587bf83c603415902ab16e8
Jeff Loiola <jloiola@mmitnetwork.com>
2022-10-13 08:29:11 -0400

    disable save btn like normal ppl

--- frontend/src/features/doc_documents/DocDocumentEditPage.tsx
+++ frontend/src/features/doc_documents/DocDocumentEditPage.tsx
@@ -44,6 +44,7 @@ export function DocDocumentEditPage() {
             Cancel
           </Button>
           <Button
+            disabled={isSaving}
             loading={isSaving}
             type="primary"
             onClick={() => {
