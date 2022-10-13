450c5dcbf3f132bf55d2ab289f194223282535ba
James Thomas <jthomas@mmitnetwork.com>
2022-10-13 13:57:34 -0400

    removed references to relevance groups, and refactored form items in create modal

--- frontend/src/features/payer-family/LocationPayerFamilyCreateModal.tsx
+++ frontend/src/features/payer-family/LocationPayerFamilyCreateModal.tsx
@@ -14,7 +14,6 @@ import {
   benefitOptions,
   planTypeOptions,
   regionOptions,
-  fieldGroupsOptions,
 } from './payerLevels';
 
 interface PayerFamilyCreateModalPropTypes {
@@ -54,24 +53,24 @@ function PayerInfo() {
     <div className="mt-4">
       <h2>Payer</h2>
       <Input.Group className="space-x-2 flex">
-        <Form.Item label="Payer Type" name={['payer_type']} className="w-48">
+        <Form.Item label="Payer Type" name={'payer_type'} className="w-48">
           <Select options={payerTypeOptions} />
         </Form.Item>
-        <Form.Item label="Payers" name={['payer_ids']} className="grow">
+        <Form.Item label="Payers" name={'payer_ids'} className="grow">
           <RemoteSelect mode="multiple" className="w-full" fetchOptions={payerOptions} />
         </Form.Item>
       </Input.Group>
       <Input.Group className="space-x-2 flex">
-        <Form.Item label="Channel" name={['channels']} className="w-full">
+        <Form.Item label="Channel" name={'channels'} className="w-full">
           <Select mode="multiple" options={channelOptions} />
         </Form.Item>
-        <Form.Item label="Benefit" name={['benefits']} className="w-full">
+        <Form.Item label="Benefit" name={'benefits'} className="w-full">
           <Select mode="multiple" options={benefitOptions} />
         </Form.Item>
-        <Form.Item label="Plan Types" name={['plan_types']} className="w-full">
+        <Form.Item label="Plan Types" name={'plan_types'} className="w-full">
           <Select mode="multiple" options={planTypeOptions} />
         </Form.Item>
-        <Form.Item label="Region" name={['regions']} className="w-full">
+        <Form.Item label="Region" name={'regions'} className="w-full">
           <Select mode="multiple" options={regionOptions} />
         </Form.Item>
       </Input.Group>
@@ -80,12 +79,6 @@ function PayerInfo() {
 }
 
 export const PayerFamilyCreateModal = (props: PayerFamilyCreateModalPropTypes) => {
-  let legacyRelevanceOptions = [
-    { label: 'Editor Manual', value: 'EDITOR_MANUAL' },
-    { label: 'Editor Automated ', value: 'EDITOR_AUTOMATED' },
-    { label: 'PAR', value: 'PAR' },
-    { label: 'N/A', value: 'N/A' },
-  ];
   const { documentType, location, onClose, onSave, open } = props;
   const [form] = useForm();
   const [getPayerFamilyByName] = useLazyGetPayerFamilyByNameQuery();
