ec4c9d85d93ca3b8b1f8154c16e7ccc8e5956490
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-14 14:58:05 -0500

    fixed DocumentFamily field so that options updates on DocumentType field change

--- frontend/src/features/doc_documents/DocDocumentInfoFormFamilyField.tsx
+++ frontend/src/features/doc_documents/DocDocumentInfoFormFamilyField.tsx
@@ -1,32 +1,77 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Form, Select, Button } from 'antd';
+import { Form, Select, Button, Spin } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { DocumentFamily } from './document_family/types';
 import { DocumentFamilyCreateModal } from './document_family/DocumentFamilyCreateModal';
-import { useGetDocumentFamiliesQuery } from './document_family/documentFamilyApi';
+import { useLazyGetDocumentFamiliesQuery } from './document_family/documentFamilyApi';
 import { useGetDocDocumentQuery } from './docDocumentApi';
 
-interface DocDocumentLocationsPropTypes {
-  onFieldChange: () => void;
+interface Option {
+  value: string;
+  label: string;
 }
+
+const useOptions = () => {
+  const form = Form.useFormInstance();
+  const documentType = Form.useWatch('document_type');
+  const [getDocumentFamilies] = useLazyGetDocumentFamiliesQuery();
+  const [options, setOptions] = useState<Option[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const selectedInOptions = useCallback(
+    (options: Option[]) => {
+      const documentFamilyId = form.getFieldValue('document_family_id');
+      return options.map((option: Option) => option.value).includes(documentFamilyId);
+    },
+    [form]
+  );
+
+  useEffect(() => {
+    setLoading(true);
+    getDocumentFamilies({ documentType }).then(({ data }) => {
+      const options =
+        data?.data.map((item: DocumentFamily) => ({
+          value: item._id,
+          label: item.name,
+        })) ?? [];
+
+      setOptions(options);
+
+      if (!selectedInOptions(options)) {
+        form.setFieldValue('document_family_id', null);
+      }
+
+      setLoading(false);
+    });
+  }, [documentType, form, getDocumentFamilies, selectedInOptions]);
+  return { options, loading };
+};
 
 export const DocDocumentInfoFormFamilyField = ({
   onFieldChange,
-}: DocDocumentLocationsPropTypes) => {
+}: {
+  onFieldChange: () => void;
+}) => {
   const { docDocumentId: docId } = useParams();
   const { data: doc } = useGetDocDocumentQuery(docId);
 
   const form = Form.useFormInstance();
   const [isVisible, setIsVisible] = useState<boolean>(false);
-  const { data } = useGetDocumentFamiliesQuery({
-    documentType: doc?.document_type,
-  });
-  const options = data?.data.map((item: DocumentFamily) => ({ value: item._id, label: item.name }));
+
+  const { options, loading } = useOptions();
   const document_family_id = Form.useWatch('document_family_id');
+
   return (
     <div>
-      <Form.Item label="Document Family" name="document_family_id">
+      <Form.Item
+        label={
+          <>
+            Document Family <Spin className="ml-1" spinning={loading} size="small" />
+          </>
+        }
+        name="document_family_id"
+      >
         <Select
           value={document_family_id}
           onSelect={(documentFamilyId: string) => {
