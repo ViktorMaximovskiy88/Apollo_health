dc04b02fd2f72e9b7a99f6cdb0bb5c6a039074d6
Jeff Loiola <jloiola@mmitnetwork.com>
2022-10-17 11:21:52 -0400

    script updates for mongosh; include today for stats

--- scripts/mongodump.sh
+++ scripts/mongodump.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # assuming local to not create oops scenario
-rm -rf ./dump
-mongodump --uri="mongodb://localhost:27017/source-hub" -u admin -p admin --authenticationDatabase=admin
+# rm -rf ./dump
+# mongodump --uri="mongodb://localhost:27017/source-hub" -u admin -p admin --authenticationDatabase=admin
 
 # careful
 # AWS_ACCESS_KEY_ID=""
 # AWS_SECRET_ACCESS_KEY=""
 # AWS_SESSION_TOKEN=""
-# mongodump --uri="mongodb+srv://apollo-dev-use1-mmit-01.3qm7h.mongodb.net/source-hub?&authSource=%24external" --authenticationMechanism=MONGODB-AWS --awsSessionToken=$AWS_SESSION_TOKEN -u $AWS_ACCESS_KEY_ID -p $AWS_SECRET_ACCESS_KEY --excludeCollection ChangeLog --excludeCollection LinkTaskLog
+# mongodump --uri="mongodb+srv://apollo-dev-use1-mmit-01.3qm7h.mongodb.net/source-hub?&authSource=%24external" --authenticationMechanism=MONGODB-AWS --awsSessionToken=$AWS_SESSION_TOKEN -u $AWS_ACCESS_KEY_ID -p $AWS_SECRET_ACCESS_KEY --excludeCollection ChangeLog
--- scripts/reset-local.sh
+++ scripts/reset-local.sh
@@ -2,11 +2,22 @@
 
 # todo format later
 COLLECTIONS=$(cat <<-END
+    db.ChangeLog.drop();
+    db.Comment.drop();
+    db.ContentExtractionResult.drop();
+    db.ContentExtractionTask.drop();
+    db.DocDocument.drop();
     db.DocumentAnalysis.drop();
+    db.LinkTaskLog.drop();
     db.RetrievedDocument.drop();
     db.SiteScrapeTask.drop();
-    db.DocDocument.drop();
 END
 )
 
 mongosh -u admin -p admin --authenticationDatabase=admin mongodb://localhost:27017/source-hub --eval "${COLLECTIONS}"
+
+# AWS_ACCESS_KEY_ID=""
+# AWS_SECRET_ACCESS_KEY=""
+# AWS_SESSION_TOKEN=""
+# AWS_SESSION_TOKEN_URL_ENCODED="" # bash doesnt do this well on mac, you can use encodeURIComponent for the same for now
+# mongosh -u $AWS_ACCESS_KEY_ID -p $AWS_SECRET_ACCESS_KEY  "mongodb+srv://apollo-dev-use1-mmit-01.3qm7h.mongodb.net/source-hub?authSource=%24external&authMechanism=MONGODB-AWS&authMechanismProperties=AWS_SESSION_TOKEN:${AWS_SESSION_TOKEN_URL_ENCODED}" --eval "${COLLECTIONS}"
