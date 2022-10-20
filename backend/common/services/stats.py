8fed74af0af8e15c78138cf8fe774ce1a8fa324f
Jeff Loiola <jloiola@mmitnetwork.com>
2022-10-20 11:55:12 -0400

    scrape

--- backend/common/services/stats.py
+++ backend/common/services/stats.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 
+from backend.common.core.enums import TaskStatus
 from backend.common.models.base_document import BaseModel
-from backend.common.models.doc_document import DocDocument
+from backend.common.models.site_scrape_task import SiteScrapeTask
 
 
 class DailyCollectionStat(BaseModel):
@@ -13,19 +14,25 @@ class DailyCollectionStat(BaseModel):
 
 async def get_collection_stats(lookback_days: int) -> list[DailyCollectionStat]:
     lookback_date = datetime.today() - timedelta(days=lookback_days)
-    docs = await DocDocument.aggregate(
+    docs = await SiteScrapeTask.aggregate(
         aggregation_pipeline=[
             {
                 "$match": {
                     "start_time": {"$gte": lookback_date},
+                    "status": {"$in": [TaskStatus.IN_PROGRESS, TaskStatus.FINISHED]},
                 }
             },
             {"$sort": {"start_time": -1}},
             {"$group": {"_id": "$site_id", "item": {"$first": "$$ROOT"}}},
+            {"$replaceRoot": {"newRoot": "$item"}},
             {
                 "$group": {
                     "_id": {"$dateToString": {"format": "%Y-%m-%d", "date": "$start_time"}},
-                    "count": {"$sum": 1},
+                    "created": {"$sum": "$new_documents_found"},
+                    "updated": {
+                        "$sum": {"$subtract": ["$documents_found", "$new_documents_found"]}
+                    },
+                    "total": {"$sum": "$documents_found"},
                 }
             },
             {"$addFields": {"name": "$_id"}},
