f7611b778dc2dd55f189658401d31fc757deff55
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-26 23:26:37 -0500

    fixed a backend bug - update lineage WIP

--- backend/app/routes/doc_documents.py
+++ backend/app/routes/doc_documents.py
@@ -143,27 +143,36 @@ async def update_doc_doc_and_new_prev_doc_doc(
     updating_doc_doc: DocDocument, new_prev_doc_doc_id: PydanticObjectId
 ):
     new_prev_doc_doc = await get_target(new_prev_doc_doc_id)
-    updating_doc_doc_mutation = {
-        "lineage_id": new_prev_doc_doc.lineage_id,
-        "previous_doc_doc_id": new_prev_doc_doc_id,
-    }
 
     if new_prev_doc_doc.is_current_version:
         await DocDocument.get_motor_collection().find_one_and_update(
             {"_id": new_prev_doc_doc_id}, {"$set": {"is_current_version": False}}
         )
-        updating_doc_doc_mutation.is_current_version: True
         await DocDocument.get_motor_collection().find_one_and_update(
-            {"_id": updating_doc_doc.id}, {"$set": updating_doc_doc_mutation}
+            {"_id": updating_doc_doc.id},
+            {
+                "$set": {
+                    "lineage_id": new_prev_doc_doc.lineage_id,
+                    "previous_doc_doc_id": new_prev_doc_doc_id,
+                    "is_current_version": True,
+                }
+            },
         )
         return
 
     await DocDocument.get_motor_collection().find_one_and_update(
-        {"_id": updating_doc_doc.id}, {"$set": updating_doc_doc_mutation}
-    )
-    await DocDocument.get_motor_collection().find_one_and_update(
         {"previous_doc_doc_id": new_prev_doc_doc_id},
         {"$set": {"previous_dod_doc_id": updating_doc_doc.id}},
+    )
+
+    await DocDocument.get_motor_collection().find_one_and_update(
+        {"_id": updating_doc_doc.id},
+        {
+            "$set": {
+                "lineage_id": new_prev_doc_doc.lineage_id,
+                "previous_doc_doc_id": new_prev_doc_doc_id,
+            }
+        },
     )
 
 
