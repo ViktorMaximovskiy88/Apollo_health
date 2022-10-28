cb5493685d23cc8e02f7a785496f26b21fb68b34
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-28 13:32:02 -0500

    fixed bugs in update_prev_document.py

--- backend/common/services/lineage/update_prev_document.py
+++ backend/common/services/lineage/update_prev_document.py
@@ -13,34 +13,17 @@ async def update_old_prev_doc_doc(updating_doc_doc: DocDocument):
     )
 
 
-async def update_ancestors_and_mark_latest(updated_doc_doc: DocDocument) -> DocDocument:
-    if updated_doc_doc._id:  # get_motor_collection returns _id
-        updated_doc_doc.id = updated_doc_doc._id
+async def update_ancestors_lineage(
+    updated_doc_doc: dict[str, any],  # dict of DocDocument
+) -> DocDocument:  # earliest ancestor
     updated_ancestor = await DocDocument.get_motor_collection().find_one_and_update(
-        {"previous_doc_doc_id": updated_doc_doc.id},
-        {"$set": {"lineage_id": updated_doc_doc.lineage_id}},
+        {"previous_doc_doc_id": updated_doc_doc["_id"]},
+        {"$set": {"lineage_id": updated_doc_doc["lineage_id"]}},
         return_document=ReturnDocument.AFTER,
     )
     if not updated_ancestor:
-        return await DocDocument.get_motor_collection().find_one_and_update(
-            {"_id": updated_doc_doc.id},
-            {"$set": {"is_current_version": True}},
-            return_document=ReturnDocument.AFTER,
-        )
-    return await update_ancestors_and_mark_latest(updated_ancestor)
-
-
-async def update_ancestors(updated_doc_doc: DocDocument) -> DocDocument:
-    if updated_doc_doc._id:  # get_motor_collection returns _id
-        updated_doc_doc.id = updated_doc_doc._id
-    updated_ancestor = await DocDocument.get_motor_collection().find_one_and_update(
-        {"previous_doc_doc_id": updated_doc_doc.id},
-        {"$set": {"lineage_id": updated_doc_doc.lineage_id}},
-        return_document=ReturnDocument.AFTER,
-    )
-    if not updated_ancestor:
-        return updated_doc_doc
-    return await update_ancestors_and_mark_latest(updated_ancestor)
+        return await DocDocument.get(updated_doc_doc["_id"])
+    return await update_ancestors_lineage(updated_ancestor)
 
 
 async def update_doc_doc_and_new_prev_doc_doc(
@@ -52,7 +35,7 @@ async def update_doc_doc_and_new_prev_doc_doc(
         await DocDocument.get_motor_collection().find_one_and_update(
             {"_id": new_prev_doc_doc_id}, {"$set": {"is_current_version": False}}
         )
-        updated_doc_doc = await DocDocument.get_motor_collection().find_one_and_update(
+        await DocDocument.get_motor_collection().find_one_and_update(
             {"_id": updating_doc_doc.id},
             {
                 "$set": {
@@ -62,7 +45,7 @@ async def update_doc_doc_and_new_prev_doc_doc(
             },
             return_document=ReturnDocument.AFTER,
         )
-        return updated_doc_doc
+        return await DocDocument.get(updating_doc_doc.id)
 
     if new_prev_doc_doc.is_current_version and not updating_doc_doc.is_current_version:
         await DocDocument.get_motor_collection().find_one_and_update(
@@ -78,15 +61,15 @@ async def update_doc_doc_and_new_prev_doc_doc(
             },
             return_document=ReturnDocument.AFTER,
         )
-        await update_ancestors_and_mark_latest(updated_doc_doc)
-        return updated_doc_doc
+        await update_ancestors_lineage(updated_doc_doc)
+        return await DocDocument.get(updating_doc_doc.id)
 
     if not new_prev_doc_doc.is_current_version and updating_doc_doc.is_current_version:
         await DocDocument.get_motor_collection().find_one_and_update(
             {"previous_doc_doc_id": new_prev_doc_doc_id},
             {"$set": {"previous_doc_doc_id": updating_doc_doc.id}},
         )
-        updated_doc_doc = await DocDocument.get_motor_collection().find_one_and_update(
+        await DocDocument.get_motor_collection().find_one_and_update(
             {"_id": updating_doc_doc.id},
             {
                 "$set": {
@@ -97,7 +80,7 @@ async def update_doc_doc_and_new_prev_doc_doc(
             },
             return_document=ReturnDocument.AFTER,
         )
-        return updated_doc_doc
+        return await DocDocument.get(updating_doc_doc.id)
 
     # neither is_current_version
     new_parent_of_inserted_documents = await DocDocument.find_one(
@@ -113,9 +96,12 @@ async def update_doc_doc_and_new_prev_doc_doc(
         },
         return_document=ReturnDocument.AFTER,
     )
-    updated_earliest_ancestor = await update_ancestors(updated_doc_doc)
+    updated_earliest_ancestor = await update_ancestors_lineage(updated_doc_doc)
+    await DocDocument.get_motor_collection().find_one_and_update(
+        {"_id": updated_earliest_ancestor.id}, {"$set": {"is_current_version": False}}
+    )
     await DocDocument.get_motor_collection().find_one_and_update(
         {"_id": new_parent_of_inserted_documents.id},
-        {"$set": {"previous_doc_doc_id": updated_earliest_ancestor}},
+        {"$set": {"previous_doc_doc_id": updated_earliest_ancestor.id}},
     )
-    return updated_doc_doc
+    return await DocDocument.get(updating_doc_doc.id)
