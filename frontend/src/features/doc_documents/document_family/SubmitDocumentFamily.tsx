564ba6013d8b84697a4918b26ebb02de2dae9bd0
Andrew Horn <ahorn@mmitnetwork.com>
2022-10-31 13:56:04 -0500

    corrected spelling mistake in component, file name

--- frontend/src/features/doc_documents/document_family/SubmitDocumentFamily.tsx
+++ frontend/src/features/doc_documents/document_family/SubmitDocumentFamily.tsx
@@ -2,7 +2,7 @@ import { Button } from 'antd';
 import { FormInstance } from 'antd/lib/form/Form';
 import { Link } from 'react-router-dom';
 
-export function SumbitDocumentFamily({ form }: { form: FormInstance<any> }) {
+export function SubmitDocumentFamily({ form }: { form: FormInstance<any> }) {
   return (
     <div className="flex items-center space-x-4">
       <Link to="/document-family">
