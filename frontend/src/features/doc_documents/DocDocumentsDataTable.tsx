92a802b46965a1718dd5d5c3b9d9985bd5e4c288
Andrew Horn <ahorn@mmitnetwork.com>
2022-11-07 11:42:43 -0600

    updated date to use function exported from common folder

--- frontend/src/features/doc_documents/DocDocumentsDataTable.tsx
+++ frontend/src/features/doc_documents/DocDocumentsDataTable.tsx
@@ -11,7 +11,7 @@ import {
   setDocDocumentTableLimit,
   setDocDocumentTableSkip,
 } from './docDocumentsSlice';
-import { prettyDateTimeFromISO, prettyFromISO } from '../../common';
+import { prettyDateTimeFromISO, prettyDateUTCFromISO } from '../../common';
 import { ButtonLink, GridPaginationToolbar } from '../../components';
 import { ChangeLogModal } from '../change-log/ChangeLogModal';
 import { Site } from '../sites/types';
@@ -29,7 +29,6 @@ import { useDataTableSort } from '../../common/hooks/use-data-table-sort';
 import { useDataTableFilter } from '../../common/hooks/use-data-table-filter';
 import { RemoteColumnFilter } from '../../components/RemoteColumnFilter';
 import { useGetSiteQuery, useGetSitesQuery, useLazyGetSitesQuery } from '../sites/sitesApi';
-import { DateTime } from 'luxon';
 
 const colors = ['magenta', 'blue', 'green', 'orange', 'purple'];
 
@@ -112,7 +111,7 @@ const useColumns = (siteNamesById: { [key: string]: string }) => {
       },
       render: ({ data: doc }: { data: DocDocument }) => {
         if (!doc.final_effective_date) return null;
-        return prettyFromISO(doc.final_effective_date, DateTime.DATE_MED, false);
+        return prettyDateUTCFromISO(doc.final_effective_date);
       },
     },
     {
