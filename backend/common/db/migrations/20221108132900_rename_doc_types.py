4b3294ed62e2285a6148eb2686f0bcf103db89e1
Jeff Loiola <jloiola@mmitnetwork.com>
2022-11-08 14:24:13 -0500

    update doc type for docanalysis and docfamily too

--- backend/common/db/migrations/20221108132900_rename_doc_types.py
+++ backend/common/db/migrations/20221108132900_rename_doc_types.py
@@ -4,6 +4,8 @@ from beanie import free_fall_migration
 
 from backend.common.models.doc_document import DocDocument
 from backend.common.models.document import RetrievedDocument
+from backend.common.models.document_family import DocumentFamily
+from backend.common.models.lineage import DocumentAnalysis
 
 
 # there were 2 :p
@@ -13,7 +15,7 @@ async def update_doc_type(from_doc_type: str, to_doc_type: str):
         {"$set": {"document_type": to_doc_type}},
     )
     logging.info(
-        f"updating docdoc doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+        f"updating DocDocument doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
     )
 
     result = await RetrievedDocument.get_motor_collection().update_many(
@@ -21,12 +23,30 @@ async def update_doc_type(from_doc_type: str, to_doc_type: str):
         {"$set": {"document_type": to_doc_type}},
     )
     logging.info(
-        f"updating retdoc doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+        f"updating RetrievedDocument doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+    )
+
+    result = await DocumentFamily.get_motor_collection().update_many(
+        {"document_type": from_doc_type},
+        {"$set": {"document_type": to_doc_type}},
+    )
+    logging.info(
+        f"updating DocumentFamily doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+    )
+
+    result = await DocumentAnalysis.get_motor_collection().update_many(
+        {"document_type": from_doc_type},
+        {"$set": {"document_type": to_doc_type}},
+    )
+    logging.info(
+        f"updating DocumentAnalysis doc type from  {from_doc_type} to {to_doc_type} -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
     )
 
 
 class Forward:
-    @free_fall_migration(document_models=[RetrievedDocument, DocDocument])
+    @free_fall_migration(
+        document_models=[RetrievedDocument, DocDocument, DocumentFamily, DocumentAnalysis]
+    )
     async def rename_doc_types(self, session):
         await update_doc_type("Covered Treatment List", "Medical Coverage List")
 
