30d8f6b08cc640654f106b0abdfd49c5724346f8
Jeff Loiola <jloiola@mmitnetwork.com>
2022-11-09 10:42:41 -0500

    doc type

---
+++ backend/cli/doc_type.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+from pathlib import Path
+
+import asyncclick as click
+
+sys.path.append(str(Path(__file__).parent.joinpath("../..").resolve()))
+from backend.common.db.init import init_db
+from backend.common.models.doc_document import DocDocument, PydanticObjectId
+from backend.common.storage.client import TextStorageClient
+from backend.scrapeworker.doc_type_matcher import DocTypeMatcher
+
+log = logging.getLogger(__name__)
+
+
+@click.group()
+@click.pass_context
+async def doc_type(ctx):
+    await init_db()
+
+
+@doc_type.command()
+@click.option("--doc-doc-id", help="Text file to analyze for doc type", required=True, type=str)
+@click.pass_context
+async def match(ctx, doc_doc_id: PydanticObjectId):
+
+    doc_storage = TextStorageClient()
+    doc_doc = await DocDocument.get(doc_doc_id)
+    location = doc_doc.locations[0]
+    raw_text = doc_storage.read_utf8_object(f"{doc_doc.text_checksum}.txt")
+    doc_type = DocTypeMatcher(raw_text, location.link_text, location.url, doc_doc.name).exec()
+    print(str(doc_type.value))
--- backend/cli/main.py
+++ backend/cli/main.py
@@ -5,6 +5,7 @@ import asyncclick as click
 
 sys.path.append(str(Path(__file__).parent.joinpath("../..").resolve()))
 from backend.cli.date_parser import date_parser
+from backend.cli.doc_type import doc_type
 from backend.cli.lineage import lineage
 from backend.cli.par import par
 from backend.cli.text_parser import text_parser
@@ -20,6 +21,7 @@ cli.add_command(lineage)
 cli.add_command(date_parser)
 cli.add_command(text_parser)
 cli.add_command(par)
+cli.add_command(doc_type)
 
 if __name__ == "__main__":
     cli(_anyio_backend="asyncio")
