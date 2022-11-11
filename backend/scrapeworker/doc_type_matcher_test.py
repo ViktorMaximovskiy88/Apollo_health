654180c85688402d7d42d7f0cc23fbe54d7dbda9
Jeff Loiola <jloiola@mmitnetwork.com>
2022-11-11 14:12:32 -0500

    best

--- backend/scrapeworker/doc_type_matcher_test.py
+++ backend/scrapeworker/doc_type_matcher_test.py
@@ -9,14 +9,14 @@ def test_contains():
 
 def test_not_contains():
     matcher = DocTypeMatcher(raw_text="", raw_link_text="", raw_url="", raw_name="")
-    result = not matcher._contains("alphabet soup dog animal", ["dog", "cat", "turtle"])
+    result = matcher._not_contains("alphabet soup dog animal", ["dog", "cat", "turtle"])
     assert result is False
 
-    result = not matcher._contains("what is turtle now", ["dog", "cat", "turtle"])
+    result = matcher._not_contains("what is turtle now", ["dog", "cat", "turtle"])
     assert result is False
 
-    result = not matcher._contains("big cat lion", ["dog", "cat", "turtle"])
+    result = matcher._not_contains("big cat lion", ["dog", "cat", "turtle"])
     assert result is False
 
-    result = not matcher._contains("lemur", ["dog", "cat", "turtle"])
+    result = matcher._not_contains("lemur", ["dog", "cat", "turtle"])
     assert result is True
