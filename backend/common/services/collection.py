73873d290facc82b604748ce98da38eece72639a
Jeremy Aldrich <jaldrich@mmitnetwork.com>
2022-11-21 10:06:46 -0500

    [APH-905] remove uneeded collected date set and instead set only from location

--- backend/common/services/collection.py
+++ backend/common/services/collection.py
@@ -297,7 +297,6 @@ class CollectionService:
         retr_doc: RetrievedDocument | None = await RetrievedDocument.find_one(
             {"_id": doc.id},
         )
-        retr_doc.first_collected_date = now
         if retr_doc.locations:
             site_loc_index: int = find_site_index(retr_doc, self.site.id)
             retr_doc.locations[site_loc_index].first_collected_date = now
@@ -311,7 +310,6 @@ class CollectionService:
         doc_doc: DocDocument | None = await DocDocument.find_one(
             {"retrieved_document_id": retr_doc.id},
         )
-        doc_doc.first_collected_date = now
         if doc_doc.locations:
             site_loc_index: int = find_site_index(doc_doc, self.site.id)
             doc_doc.locations[site_loc_index].first_collected_date = now
