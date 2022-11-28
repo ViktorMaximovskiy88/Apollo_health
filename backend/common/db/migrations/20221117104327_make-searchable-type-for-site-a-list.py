236ce29169159907fde778072057a9eda53e84bc
James Thomas <jthomas@mmitnetwork.com>
2022-11-28 12:24:01 -0500

    combined two migrations into 1

--- backend/common/db/migrations/20221117104327_make-searchable-type-for-site-a-list.py
+++ backend/common/db/migrations/20221117104327_make-searchable-type-for-site-a-list.py
@@ -1,12 +1,31 @@
 from beanie import free_fall_migration
 
 from backend.common.models.site import Site
+from backend.common.models.site_scrape_task import SiteScrapeTask
 
 
 class Forward:
     @free_fall_migration(document_models=[Site])
     async def make_searchable_type_for_site_a_list(self, session):
         await Site.get_motor_collection().update_many(
+            {"scrape_method_configuration.searchable_type": {"$in": ["CPTCODES", "JCODES"]}},
+            [
+                {
+                    "$set": {
+                        "scrape_method_configuration.searchable_type": [
+                            "$scrape_method_configuration.searchable_type"
+                        ]
+                    }
+                }
+            ],
+        )
+
+        await Site.get_motor_collection().update_many(
+            {"scrape_method_configuration.searchable_type": None},
+            {"scrape_method_configuration.searchable_type": []},
+        )
+
+        await SiteScrapeTask.get_motor_collection().update_many(
             {},
             [
                 {
@@ -24,6 +43,17 @@ class Backward:
     @free_fall_migration(document_models=[Site])
     async def make_searchable_type_for_site_a_str(self, session):
         await Site.get_motor_collection().update_many(
+            {},
+            {
+                "$set": {
+                    "scrape_method_configuration.searchable_type": {
+                        "$scrape_method_configuration.searchable_type"
+                    }
+                }
+            },
+        )
+
+        await SiteScrapeTask.get_motor_collection().update_many(
             {},
             {
                 "$set": {
