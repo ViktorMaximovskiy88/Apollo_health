04dbd7cc73c7f93990c4188f1cc80e098c34bd54
Matthew Brown <mbrown@panalgo.com>
2022-12-05 18:31:09 +0000

    Merged PR 9634: APH-817 migration fix for SiteScrapeTask
    APH-817 migration fix for SiteScrapeTask

---
+++ backend/common/db/migrations/20221130140228_make_searchable_type_for_site_a_list_fix.py
@@ -0,0 +1,28 @@
+from beanie import free_fall_migration
+
+from backend.common.models.site import Site
+from backend.common.models.site_scrape_task import SiteScrapeTask
+
+
+class Forward:
+    @free_fall_migration(document_models=[Site])
+    async def make_searchable_type_for_site_a_list(self, session):
+        await SiteScrapeTask.get_motor_collection().update_many(
+            {"scrape_method_configuration.searchable_type": None},
+            {"$set": {"scrape_method_configuration.searchable_type": []}},
+        )
+
+
+class Backward:
+    @free_fall_migration(document_models=[Site])
+    async def make_searchable_type_for_site_a_str(self, session):
+        await SiteScrapeTask.get_motor_collection().update_many(
+            {},
+            {
+                "$set": {
+                    "scrape_method_configuration.searchable_type": {
+                        "$scrape_method_configuration.searchable_type.0"
+                    }
+                }
+            },
+        )
