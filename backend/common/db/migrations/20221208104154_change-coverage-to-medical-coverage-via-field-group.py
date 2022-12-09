9dc2a90ee89aaec28e928b2d90e3ef76dd63823c
James Thomas <jthomas@mmitnetwork.com>
2022-12-09 10:58:36 -0500

    migration for fieldGroups

---
+++ backend/common/db/migrations/20221208104154_change-coverage-to-medical-coverage-via-field-group.py
@@ -0,0 +1,22 @@
+from beanie import free_fall_migration
+
+from backend.common.models.document_family import DocumentFamily
+
+
+class Forward:
+    @free_fall_migration(document_models=[DocumentFamily])
+    async def change_coverage_to_medical_coverage_via_field_group(self, session):
+        await DocumentFamily.get_motor_collection().update_many(
+            {"field_groups": {"$exists": True}},
+            {"$set": {"field_groups.$[element]": "MEDICAL_COVERAGE"}},
+            {"arrayFilters": [{"element": "COVERAGE"}], "upsert": True},
+        )
+
+
+class Backward:
+    @free_fall_migration(document_models=[DocumentFamily])
+    async def change_coverage_to_medical_coverage_via_field_group(self, session):
+        await DocumentFamily.get_motor_collection().update_many(
+            {"field_groups": {"$exists": True}},
+            {"$set": {"field_groups.$[].MEDICAL_COVERAGE": "COVERAGE"}},
+        )
