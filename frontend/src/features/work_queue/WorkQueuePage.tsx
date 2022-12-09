0e80b9ab8d17bf765a5c1254dd7a37c192791a91
Andrew Horn <ahorn@mmitnetwork.com>
2022-12-09 19:37:02 +0000

    Merged PR 9764: [APH-991 & APH-1003] Add Final Effective Date and First Collected Date columns to Work Queue Classification table
    - added final effective date to classification queue table
    - added first collected date to classification table
    - updated column midWidths
    
    ![Screen Shot 2022-12-09 at 11.05.32 AM (2).png](https://dev.azure.com/mmitdev/f19f6738-e2ca-474f-9016-32bcaa11559a/_apis/git/repositories/094a4319-8808-4aec-a13d-04a20c4700e9/pullRequests/9764/attachments/Screen%20Shot%202022-12-09%20at%2011.05.32%20AM%20%282%29.png)

--- frontend/src/features/work_queue/WorkQueuePage.tsx
+++ frontend/src/features/work_queue/WorkQueuePage.tsx
@@ -1,24 +1,17 @@
 import ReactDataGrid from '@inovua/reactdatagrid-community';
-import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter';
 import { Button, notification } from 'antd';
 import { useCallback, useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { BaseDocument } from '../../common';
-import { dateDuration } from '../../common/date';
 import {
   useDataTableFilter,
   useDataTablePagination,
   useDataTableSort,
   useInterval,
 } from '../../common/hooks';
-import { ButtonLink } from '../../components/ButtonLink';
-import { DocDocument, TaskLock } from '../doc_documents/types';
-import { useGetUsersQuery } from '../users/usersApi';
+import { DocDocument } from '../doc_documents/types';
 import { useGetWorkQueueQuery } from './workQueuesApi';
 import { MainLayout } from '../../components';
-import { DocumentTypes } from '../retrieved_documents/types';
-import { useGetSiteQuery, useGetSitesQuery, useLazyGetSitesQuery } from '../sites/sitesApi';
-import { RemoteColumnFilter } from '../../components/RemoteColumnFilter';
+import { useGetSitesQuery } from '../sites/sitesApi';
 import {
   workQueueTableState,
   setWorkQueueTableFilter,
@@ -31,24 +24,7 @@ import {
   useLazyGetWorkQueueItemsQuery,
   useTakeNextWorkItemMutation,
 } from '../doc_documents/docDocumentApi';
-
-function useSiteSelectOptions() {
-  const [getSites] = useLazyGetSitesQuery();
-  const siteOptions = useCallback(
-    async (search: string) => {
-      const { data } = await getSites({
-        limit: 20,
-        skip: 0,
-        sortInfo: { name: 'name', dir: 1 },
-        filterValue: [{ name: 'name', operator: 'contains', type: 'string', value: search }],
-      });
-      if (!data) return [];
-      return data.data.map((site) => ({ label: site.name, value: site._id }));
-    },
-    [getSites]
-  );
-  return { siteOptions };
-}
+import { useWorkQueueColumns } from './useWorkQueueColumns';
 
 function useGetSiteNamesById() {
   const [siteIds, setSiteIds] = useState<string[]>([]);
@@ -72,102 +48,6 @@ function uniqueSiteIds(items: DocDocument[]) {
   const usedSiteIds: { [key: string]: boolean } = {};
   items.forEach((item) => item.locations.forEach((l) => (usedSiteIds[l.site_id] = true)));
   return Object.keys(usedSiteIds);
-}
-
-function useWorkQueueColumns(
-  queueId: string | undefined,
-  siteNamesById: { [key: string]: string }
-) {
-  const { data: users } = useGetUsersQuery();
-  const { siteOptions } = useSiteSelectOptions();
-
-  const res = useSelector(workQueueTableState);
-  const siteFilter = res.filter.find((f) => f.name === 'locations.site_id');
-  const { data: site } = useGetSiteQuery(siteFilter?.value, { skip: !siteFilter?.value });
-  const initialOptions = site ? [{ value: site._id, label: site.name }] : [];
-  const columns = [
-    {
-      name: 'name',
-      header: 'Name',
-      defaultFlex: 1,
-      minWidth: 300,
-      render: ({ data: item }: { data: { _id: string; name: string } }) => {
-        return <ButtonLink to={`${item._id}/process`}>{item.name}</ButtonLink>;
-      },
-    },
-    {
-      name: 'locations.link_text',
-      header: 'Link Text',
-      defaultFlex: 1,
-      minWidth: 300,
-      render: ({ data }: { data: { locations: { link_text: string }[] } }) => {
-        return data.locations.map((s) => s.link_text).join(', ');
-      },
-    },
-    {
-      name: 'locations.site_id',
-      header: 'Sites',
-      filterEditor: RemoteColumnFilter,
-      filterEditorProps: {
-        fetchOptions: siteOptions,
-        initialOptions,
-      },
-      defaultFlex: 1,
-      render: ({ data }: { data: { locations: { site_id: string }[] } }) => {
-        return data.locations.map((s) => siteNamesById[s.site_id]).join(', ');
-      },
-    },
-    {
-      name: 'locks.user_id',
-      header: 'Assignee',
-      defaultFlex: 1,
-      minWidth: 300,
-      filterEditor: SelectFilter,
-      filterEditorProps: {
-        placeholder: 'All',
-        dataSource:
-          users?.map((u) => ({
-            id: u._id,
-            label: u.full_name,
-          })) || [],
-      },
-      render: ({ data: item }: { data: { _id: string; locks: TaskLock[] } }) => {
-        if (!users) return;
-        const lock = item.locks.find((l) => l.work_queue_id === queueId);
-        if (lock) {
-          const tillExpiry = dateDuration(lock.expires).toMillis();
-          if (tillExpiry < 0) {
-            const u = users.find((u) => u._id === lock.user_id);
-            if (u) return u.full_name;
-          }
-        }
-      },
-    },
-    {
-      name: 'document_type',
-      header: 'Document Type',
-      defaultFlex: 1,
-      filterEditor: SelectFilter,
-      filterEditorProps: {
-        placeholder: 'All',
-        dataSource: DocumentTypes,
-      },
-      render: ({ value: document_type }: { value: string }) => {
-        return <>{document_type}</>;
-      },
-    },
-    {
-      header: 'Actions',
-      render: ({ data: item }: { data: BaseDocument }) => {
-        return (
-          <ButtonLink type="default" to={`${item._id}/process`}>
-            Take
-          </ButtonLink>
-        );
-      },
-    },
-  ];
-  return { columns };
 }
 
 export function WorkQueuePage() {
