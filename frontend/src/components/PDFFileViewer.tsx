6133bfe427ce987b9b2646810f8684a7ebc9c265
James Thomas <jthomas@mmitnetwork.com>
2022-12-12 02:33:37 -0500

    add error boundary fallback component with download button for broken file

--- frontend/src/components/PDFFileViewer.tsx
+++ frontend/src/components/PDFFileViewer.tsx
@@ -1,19 +1,37 @@
 import { Viewer, PageChangeEvent } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import { useGetDocumentViewerUrlQuery } from '../features/retrieved_documents/documentsApi';
-
+import { ErrorBoundary } from 'react-error-boundary';
+import { Button } from 'antd';
 export function PDFFileLoader({ docId, onPageChange }: { docId?: string; onPageChange: Function }) {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const { data } = useGetDocumentViewerUrlQuery(docId, { refetchOnMountOrArgChange: true });
+
   if (!data) return null;
 
+  const ViewerErrorFallback = () => {
+    return (
+      <div className="flex flex-col justify-start items-start">
+        <span className="mr-2 mb-2">Error Loading File, please download to view</span>
+
+        <Button type="primary" danger>
+          <a href={data.url} download>
+            Download
+          </a>
+        </Button>
+      </div>
+    );
+  };
+
   return (
-    <Viewer
-      fileUrl={data.url}
-      plugins={[defaultLayoutPluginInstance]}
-      onPageChange={(e: PageChangeEvent) => {
-        onPageChange(e.currentPage);
-      }}
-    />
+    <ErrorBoundary FallbackComponent={ViewerErrorFallback}>
+      <Viewer
+        fileUrl={data.url}
+        plugins={[defaultLayoutPluginInstance]}
+        onPageChange={(e: PageChangeEvent) => {
+          onPageChange(e.currentPage);
+        }}
+      />
+    </ErrorBoundary>
   );
 }
