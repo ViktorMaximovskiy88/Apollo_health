3efb749d1c1bcae3776e130bbfe8d6204d1cdca7
James Thomas <jthomas@mmitnetwork.com>
2022-12-19 00:30:14 -0500

    create DocumentFamilyEditDrawer Component

--- frontend/src/features/doc_documents/document_family/DocumentFamilyHomePage.tsx
+++ frontend/src/features/doc_documents/document_family/DocumentFamilyHomePage.tsx
@@ -3,12 +3,27 @@ import { useState } from 'react';
 import { MainLayout } from '../../../components';
 import { DocumentFamilyCreateDrawer } from './DocumentFamilyCreateDrawer';
 import { DocumentFamilyTable } from './DocumentFamilyDataTable';
+import { DocumentFamilyEditDrawer } from './DocumentFamilyEditDrawer';
 
 export function DocumentFamilyHomePage() {
   const [open, setOpen] = useState(false);
+  const [openEditDrawer, setOpenEditDrawer] = useState(false);
+  const [docFamilyId, setDocFamilyId] = useState('');
+
   return (
     <MainLayout sectionToolbar={<Button onClick={() => setOpen(true)}>Create</Button>}>
-      <DocumentFamilyTable />
+      <DocumentFamilyTable setDocFamilyId={setDocFamilyId} setOpenEditDrawer={setOpenEditDrawer} />
+      <DocumentFamilyEditDrawer
+        mask={true}
+        docFamilyId={docFamilyId}
+        open={openEditDrawer}
+        onSave={() => {
+          setOpenEditDrawer(false);
+        }}
+        onClose={() => {
+          setOpenEditDrawer(false);
+        }}
+      />
       <DocumentFamilyCreateDrawer
         open={open}
         onSave={() => {
