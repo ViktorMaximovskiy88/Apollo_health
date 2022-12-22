49299523d8de58779ad0eb7a53907de9af399931
James Thomas <jthomas@mmitnetwork.com>
2022-12-22 10:36:04 -0500

    Add migration for existing Searchables and to change doc type

---
+++ backend/common/db/migrations/20221222102614_change-doc-type-medical-coverage-list-for-searchables.py
@@ -0,0 +1,28 @@
+from beanie import free_fall_migration
+
+from backend.common.models.doc_document import DocDocument
+from backend.common.models.search_codes import SearchCodeSet
+
+
+class Forward:
+    @free_fall_migration(document_models=[DocDocument, SearchCodeSet])
+    async def searchable_docs(self):
+        codes: set[str] = set()
+        async for code_set in SearchCodeSet.find_all():
+            codes = codes.union(code_set.codes)
+
+        DocDocument.find({"name": {"$in": codes}}).update(
+            {"$set": {"document_type": "Medical Coverage List"}}
+        )
+
+
+class Backward:
+    @free_fall_migration(document_models=[DocDocument, SearchCodeSet])
+    async def searchable_docs(self):
+        codes: set[str] = set()
+        async for code_set in SearchCodeSet.find_all():
+            codes = codes.union(code_set.codes)
+
+        DocDocument.find({"name": {"$in": codes}}).update(
+            {"$set": {"document_type": "Authorization Policy"}}
+        )
