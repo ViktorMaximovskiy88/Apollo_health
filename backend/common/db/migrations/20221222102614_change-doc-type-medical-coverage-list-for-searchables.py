a9554d85139bd58bcdca715109903340c705c1ff
James Thomas <jthomas@mmitnetwork.com>
2022-12-22 17:03:29 -0500

    updated migration

--- backend/common/db/migrations/20221222102614_change-doc-type-medical-coverage-list-for-searchables.py
+++ backend/common/db/migrations/20221222102614_change-doc-type-medical-coverage-list-for-searchables.py
@@ -2,17 +2,21 @@ from beanie import free_fall_migration
 
 from backend.common.models.doc_document import DocDocument
 from backend.common.models.search_codes import SearchCodeSet
+from backend.common.models.site import Site
 
 
 class Forward:
-    @free_fall_migration(document_models=[DocDocument, SearchCodeSet])
+    @free_fall_migration(document_models=[DocDocument, SearchCodeSet, Site])
     async def searchable_docs(self):
         codes: set[str] = set()
         async for code_set in SearchCodeSet.find_all():
             codes = codes.union(code_set.codes)
 
-        DocDocument.find({"name": {"$in": codes}}).update(
-            {"$set": {"document_type": "Medical Coverage List"}}
+        searchable_sites = await Site.get_motor_collection().find_all({"searchable": "True"})
+
+        DocDocument.find({"name": {"$in": codes}}).update_many(
+            {"$locations.site_id": {"$in": searchable_sites}},
+            {"$set": {"document_type": "Medical Coverage List"}},
         )
 
 
@@ -23,6 +27,9 @@ class Backward:
         async for code_set in SearchCodeSet.find_all():
             codes = codes.union(code_set.codes)
 
-        DocDocument.find({"name": {"$in": codes}}).update(
-            {"$set": {"document_type": "Authorization Policy"}}
+        searchable_sites = await Site.get_motor_collection().find_all({"searchable": "True"})
+
+        DocDocument.find({"name": {"$in": codes}}).update_many(
+            {"$locations.site_id": {"$in": searchable_sites}},
+            {"$set": {"document_type": "Authorization Policy"}},
         )
