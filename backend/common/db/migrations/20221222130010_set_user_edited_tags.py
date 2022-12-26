2cc8899d6516edae80308a7ad562e21d69347fe6
Jeff Loiola <jloiola@mmitnetwork.com>
2022-12-26 08:45:01 -0500

    oops

--- backend/common/db/migrations/20221222130010_set_user_edited_tags.py
+++ backend/common/db/migrations/20221222130010_set_user_edited_tags.py
@@ -1,12 +1,13 @@
-from pprint import pprint
+import logging
 
 from beanie import free_fall_migration
+from pymongo import UpdateOne
 
 from backend.common.models.change_log import ChangeLog
 from backend.common.models.doc_document import DocDocument
 
 
-async def find_changes(tag_type: str):
+async def find_changes(tag_type: str, output: dict):
     match = {
         "collection": "DocDocument",
         "delta.path": {"$regex": rf"/{tag_type}/\d/focus", "$options": "i"},
@@ -27,9 +28,14 @@ async def find_changes(tag_type: str):
         ]
     ).to_list()
 
-    # kinda brutal...
-    # looking for shortcut hueristic...
-    pprint(changed_docs)
+    # semi redundant with the group above ...
+    for change_doc in changed_docs:
+        key = change_doc["_id"]
+        if key not in output:
+            output[key] = set()
+        output[key].add(tag_type)
+
+    return output
 
 
 class Forward:
@@ -37,8 +43,19 @@ class Forward:
     async def set_user_edited_fields(self, session):
 
         # first get code for changed tags by doc with log entries
-        await find_changes("therapy_tags")
-        await find_changes("indication_tags")
+        docs = {}
+        docs = await find_changes("therapy_tags", docs)
+        docs = await find_changes("indication_tags", docs)
+
+        batch = []
+        for id, fields in docs.items():
+            update = {"user_edited_fields": {"$each": list(fields)}}
+            batch.append(UpdateOne({"_id": id}, {"$addToSet": update}))
+
+        result = await DocDocument.get_motor_collection().bulk_write(batch)
+        logging.info(
+            f"bulk_write -> acknowledged={result.acknowledged} matched_count={result.matched_count} modified_count={result.modified_count}"  # noqa
+        )
 
 
 class Backward:
