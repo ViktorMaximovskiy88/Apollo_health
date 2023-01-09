5dbc0534450551a6c48ebea929cdde38dec09db0
Jeremy Aldrich <jaldrich@mmitnetwork.com>
2023-01-09 15:53:01 -0500

    [APH-1087] remove legacy site id check from cancel manual scrape

--- frontend/src/features/collections/CollectionsPage.tsx
+++ frontend/src/features/collections/CollectionsPage.tsx
@@ -74,34 +74,32 @@ export function ManualCollectionButton(props: any) {
   }
 
   async function handleCancelManualScrape() {
-    if (site?._id) {
-      try {
-        setIsLoading(true);
-        let response: any = await cancelAllScrapes(site!._id);
-        if (response.data?.success) {
-          refreshDocs();
-          setIsLoading(false);
-        } else {
-          setIsLoading(false);
-          notification.error({
-            message: 'Please review and update the following documents',
-            description: response.error.data.detail,
-          });
-        }
-      } catch (err) {
-        if (isErrorWithData(err)) {
-          setIsLoading(false);
-          notification.error({
-            message: 'Error Cancelling Collection',
-            description: `${err.data.detail}`,
-          });
-        } else {
-          setIsLoading(false);
-          notification.error({
-            message: 'Error Cancelling Collection',
-            description: 'Unknown error.',
-          });
-        }
+    try {
+      setIsLoading(true);
+      let response: any = await cancelAllScrapes(site!._id);
+      if (response.data?.success) {
+        refreshDocs();
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        notification.error({
+          message: 'Please review and update the following documents',
+          description: response.error.data.detail,
+        });
+      }
+    } catch (err) {
+      if (isErrorWithData(err)) {
+        setIsLoading(false);
+        notification.error({
+          message: 'Error Cancelling Collection',
+          description: `${err.data.detail}`,
+        });
+      } else {
+        setIsLoading(false);
+        notification.error({
+          message: 'Error Cancelling Collection',
+          description: 'Unknown error.',
+        });
       }
     }
   }
