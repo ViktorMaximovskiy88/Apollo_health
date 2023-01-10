0888a3f1843de6efb79e35a1f1ade9d23361b18b
Jeff Loiola <jloiola@mmitnetwork.com>
2023-01-10 07:21:55 -0500

    task log too

--- backend/app/scripts/ensure_indices.py
+++ backend/app/scripts/ensure_indices.py
@@ -11,8 +11,13 @@ from backend.common.db.init import get_motor_db, init_db
 async def ensure_indices():
     db = get_motor_db()
     # LinktaskLog
-    expire_after_seconds = 2580000  # 30 days
+    expire_after_seconds = 30 * 86400  # 30 days
     await db.LinkTaskLog.create_index(
+        "created_at", expireAfterSeconds=expire_after_seconds, background=True
+    )
+
+    expire_after_seconds = 10 * 86400  # 10 days
+    await db.TaskLog.create_index(
         "created_at", expireAfterSeconds=expire_after_seconds, background=True
     )
 
