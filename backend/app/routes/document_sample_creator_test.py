a945317f830761b9c29325855dc043cfd3218728
Jeff Loiola <jloiola@mmitnetwork.com>
2023-01-11 11:31:16 -0500

    using fitz for reading

--- backend/app/routes/document_sample_creator_test.py
+++ backend/app/routes/document_sample_creator_test.py
@@ -1,7 +1,6 @@
-from io import BufferedReader
 from pathlib import Path
 
-import pdfplumber
+import fitz
 import pytest
 
 from backend.app.routes.document_sample_creator import DocumentSampleCreator
@@ -9,17 +8,18 @@ from backend.app.routes.document_sample_creator import DocumentSampleCreator
 
 @pytest.mark.asyncio
 async def test_take_document_sample():
-    input = Path(__file__).parent.joinpath("mocks", "pdf_fixture.pdf")
-    with open(input, "rb") as infile:
-        with pdfplumber.open(infile) as pdf:
-            first_page_text = pdf.pages[0].extract_text()
+    input_file = Path(__file__).parent.joinpath("mocks", "pdf_fixture.pdf")
+
+    with fitz.open(input_file) as input_doc:
+        for page in input_doc.pages(start=0, stop=1):
+            first_page_text = page.get_text()
 
         sampler = DocumentSampleCreator()
-        sample = sampler.sample_file(infile)
-        sample.seek(0)
+        sample_bytes = sampler.sample_file(stream=input_doc.write())
 
         all_sample_text = ""
-        with pdfplumber.open(BufferedReader(sample)) as pdf:  # type: ignore
-            for page in pdf.pages:
-                all_sample_text += page.extract_text()
+        with fitz.open(stream=sample_bytes) as pdf:  # type: ignore
+            for page in pdf.pages():
+                all_sample_text += page.get_text()
+
         assert all_sample_text == first_page_text
