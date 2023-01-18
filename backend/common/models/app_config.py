678af189d60b65c0b07c32a0ca411a82942ff7f8
Jeff Loiola <jloiola@mmitnetwork.com>
2023-01-18 17:03:00 -0500

    fixed call and type

--- backend/common/models/app_config.py
+++ backend/common/models/app_config.py
@@ -7,8 +7,9 @@ from backend.common.models.base_document import BaseDocument
 
 class AppConfig(BaseDocument):
     key: Indexed(str)
-    data: dict[str, Any]
+    data: dict[str, Any] | list[Any]
 
     @classmethod
-    async def get_tricare_tokens(cls):
-        return await cls.find({"key": "tricare_tokens"}).to_list()
+    async def get_tricare_tokens(cls) -> list[str]:
+        result = await cls.find_one({"key": "tricare_tokens"})
+        return result.data
