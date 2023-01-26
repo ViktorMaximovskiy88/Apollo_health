3b511e19d1cd79723b4bd6be533735cea6edd838
James Thomas <jthomas@mmitnetwork.com>
2023-01-26 17:08:49 -0500

    moved setting payer_type up to payerFamily create drawer

--- frontend/src/features/payer-family/LocationPayerFamilyCreateDrawer.tsx
+++ frontend/src/features/payer-family/LocationPayerFamilyCreateDrawer.tsx
@@ -4,7 +4,7 @@ import { DocDocumentLocation } from '../doc_documents/locations/types';
 import { useAddPayerFamilyMutation, useLazyConvertPayerFamilyDataQuery } from './payerFamilyApi';
 import { useForm } from 'antd/lib/form/Form';
 import { Rule } from 'antd/lib/form';
-import { ReactNode, useCallback, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { PayerFamily } from './types';
 import { PayerFamilyInfoForm } from './PayerFamilyInfoForm';
 import { CloseOutlined, WarningFilled } from '@ant-design/icons';
@@ -108,6 +108,10 @@ export const PayerFamilyCreateDrawer = (props: PayerFamilyCreateDrawerPropTypes)
     },
     [form]
   );
+
+  useEffect(() => {
+    form.setFieldValue('payer_type', 'Not Selected');
+  }, [form]);
 
   return (
     <Drawer
--- frontend/src/features/payer-family/PayerFamilyInfoForm.tsx
+++ frontend/src/features/payer-family/PayerFamilyInfoForm.tsx
@@ -142,10 +142,6 @@ export function MatchingPlansModal() {
 export const PayerFamilyInfoForm = () => {
   const form = Form.useFormInstance();
 
-  useEffect(() => {
-    form.setFieldValue('payer_type', 'Not Selected');
-  }, [form]);
-
   const handlePlanChange = useCallback(() => {
     form.setFieldsValue({ payer_ids: [] });
   }, [form]);
@@ -184,6 +180,7 @@ export const PayerFamilyInfoForm = () => {
 };
 
 function backboneValueValidator(payerValue: string[], payerType: string | null) {
+  console.log(payerValue, payerType);
   if (payerValue.length === 0 && payerType !== 'Not Selected') {
     return Promise.reject('Backbone value is required');
   }
