bfa27fff4bb568e1c05324f31e34f7ab1cad6dff
Harmandeep Singh <hsingh@mmitnetwork.com>
2023-02-08 09:57:41 -0500

    added more test cases

--- backend/app/routes/table_query_test.py
+++ backend/app/routes/table_query_test.py
@@ -1,12 +1,24 @@
+from datetime import datetime
 from random import random
 
 import pytest
 import pytest_asyncio
 from beanie import PydanticObjectId
+from pydantic import HttpUrl
 
-from backend.app.routes.table_query import TableFilterInfo, TableSortInfo, _prepare_table_query
+from backend.app.routes.table_query import (
+    TableFilterInfo,
+    TableSortInfo,
+    _prepare_table_query,
+    transform_value,
+)
+from backend.common.core.enums import ApprovalStatus, CollectionMethod, ScrapeMethod, SiteStatus
 from backend.common.db.init import init_db
+from backend.common.models.doc_document import DocDocument
+from backend.common.models.document import RetrievedDocument
 from backend.common.models.document_family import DocumentFamily
+from backend.common.models.shared import DocDocumentLocation, RetrievedDocumentLocation
+from backend.common.models.site import BaseUrl, ScrapeMethodConfiguration, Site
 from backend.common.models.user import User
 
 
@@ -42,6 +54,100 @@ class MockLogger:
 @pytest_asyncio.fixture()
 async def logger():
     return MockLogger()
+
+
+def simple_site(
+    disabled=False,
+    base_urls=[BaseUrl(url=HttpUrl("https://www.example.com/", scheme="https"), status="ACTIVE")],
+    collection_method=CollectionMethod.Automated,
+    status=SiteStatus.ONLINE,
+    last_run_status=None,
+) -> Site:
+    return Site(
+        name="Test",
+        collection_method=collection_method,
+        scrape_method=ScrapeMethod.Simple,
+        scrape_method_configuration=ScrapeMethodConfiguration(
+            document_extensions=[],
+            url_keywords=[],
+            proxy_exclusions=[],
+            follow_links=False,
+            follow_link_keywords=[],
+            follow_link_url_keywords=[],
+        ),
+        disabled=disabled,
+        last_run_status=last_run_status,
+        status=status,
+        cron="0 * * * *",
+        base_urls=base_urls,
+    )
+
+
+def simple_ret_doc(site: Site) -> RetrievedDocument:
+    doc = RetrievedDocument(
+        name="test",
+        checksum="test",
+        text_checksum="test",
+        first_collected_date=datetime.now(),
+        last_collected_date=datetime.now(),
+        doc_type_match=None,
+        locations=[
+            RetrievedDocumentLocation(
+                site_id=site.id,
+                first_collected_date=datetime.now(),
+                last_collected_date=datetime.now(),
+                url="https://www.example.com/doc",
+                base_url="https://www.example.com/",
+                link_text="",
+                closest_heading="",
+                siblings_text=None,
+            )
+        ],
+    )
+    return doc
+
+
+def simple_doc_doc(site: Site, ret_doc: RetrievedDocument, i: int) -> DocDocument:
+    doc = DocDocument(
+        name=f"test{i}",
+        checksum=f"test{i}",
+        text_checksum=f"test{i}",
+        document_type="Treatment Request Form",
+        first_collected_date=datetime.now(),
+        retrieved_document_id=ret_doc.id,
+        family_status=ApprovalStatus.APPROVED,
+        last_collected_date=datetime.now(),
+        last_updated_date=datetime.now(),
+        document_family_id=PydanticObjectId(),
+        classification_status=ApprovalStatus.APPROVED,
+        locations=[
+            DocDocumentLocation(
+                site_id=site.id,  # type: ignore
+                first_collected_date=datetime.now(),
+                last_collected_date=datetime.now(),
+                url="https://www.example.com/doc",
+                base_url="https://www.example.com/",
+                link_text="",
+                closest_heading="",
+                siblings_text=None,
+            )
+        ],
+    )
+    return doc
+
+
+async def populate_db():
+
+    await init_db(mock=True)
+    site = simple_site()
+    await site.save()
+    ret_doc = simple_ret_doc(site)
+    await ret_doc.save()
+    docs: list[RetrievedDocument] = []
+    for i in range(10):
+        doc = simple_doc_doc(site, ret_doc, i)
+        await doc.save()
+    return docs
 
 
 def test_prepare_table_query_different_value_type():
@@ -94,6 +200,7 @@ def test_prepare_table_query_no_filter_value_and_operator_in_list():
     ]
 
 
+# ["empty", "notEmpty", "leq","notinlist","neq"]
 def test_prepare_table_query_no_filter_value_and_operator_not_in_list():
     filters = [
         TableFilterInfo(name="document_type", operator="textcontains", type="select", value=None),
@@ -101,3 +208,33 @@ def test_prepare_table_query_no_filter_value_and_operator_not_in_list():
     sorts = [TableSortInfo(name="name", dir=-1)]
     match, sort_by = _prepare_table_query(sorts, filters)
     assert match == []
+
+
+def test_prepare_table_query_value_is_None():
+    filters = [
+        TableFilterInfo(name="document_type", operator="empty", type="select", value=None),
+    ]
+    sorts = [TableSortInfo(name="name", dir=-1)]
+    match, sort_by = _prepare_table_query(sorts, filters)
+    assert match == [{"document_type": {"$in": [None, ""]}}]
+
+
+def test_transform_value_boolean():
+    value1 = "True"
+    value2 = "False"
+    type = "boolean"
+    resp1 = transform_value(value1, type)
+    assert resp1 is True
+    resp2 = transform_value(value2, type)
+    assert resp2 is False
+
+
+def test_transform_value_date_number():
+    value = "2023-02-08"
+    value1 = "1"
+    type = "date"
+    type1 = "number"
+    resp = transform_value(value, type)
+    resp1 = transform_value(value1, type1)
+    assert resp == datetime(2023, 2, 8)
+    assert resp1 == 1
