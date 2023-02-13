feba99339bad5db9781e188a743b35fb2e620310
James Thomas <jthomas@mmitnetwork.com>
2023-02-13 12:38:00 -0500

    modify migration to run new work queues

--- backend/common/db/migrations/20230210093914_add_requires_comment_to_work_queues.py
+++ backend/common/db/migrations/20230210093914_add_requires_comment_to_work_queues.py
@@ -1,39 +1,14 @@
 from beanie import free_fall_migration
 
+from backend.app.scripts.create_work_queues import execute
 from backend.common.models.work_queue import WorkQueue
 
 
 class Forward:
     @free_fall_migration(document_models=[WorkQueue])
-    async def add_requires_comment_to_doc_and_payer_fam(self, session):
-        await WorkQueue.get_motor_collection().update_many(
-            {"submit_actions": {"$elemMatch": {"label": "Reject Classification"}}},
-            {"$set": {"submit_actions.$[element].require_comment": True}},
-            False,
-            [{"element.label": "Reject Classification"}],
-        )
-
-        await WorkQueue.get_motor_collection().update_many(
-            {"submit_actions": {"$elemMatch": {"label": "Reject Family"}}},
-            {"$set": {"submit_actions.$[element].require_comment": True}},
-            False,
-            [{"element.label": "Reject Family"}],
-        )
+    async def update_work_queue(self, session):
+        await execute()
 
 
-class Backward:
-    @free_fall_migration(document_models=[WorkQueue])
-    async def removes_requires_comment_to_doc_and_payer_fam(self, session):
-        await WorkQueue.get_motor_collection().update_many(
-            {"submit_actions": {"$elemMatch": {"label": "Reject Classification"}}},
-            {"$unset": {"submit_actions.$[element].require_comment": ""}},
-            False,
-            [{"element.label": "Reject Classification"}],
-        )
-
-        await WorkQueue.get_motor_collection().update_many(
-            {"submit_actions": {"$elemMatch": {"label": "Reject Family"}}},
-            {"$unset": {"submit_actions.$[element].require_comment": ""}},
-            False,
-            [{"element.label": "Reject Family"}],
-        )
+class Backend:
+    ...
