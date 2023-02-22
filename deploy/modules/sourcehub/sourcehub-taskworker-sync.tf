4750b247a9321d58fb18879e8df79a1ee8322885
Jeff Loiola <jloiola@mmitnetwork.com>
2023-02-22 13:50:21 +0000

    Merged PR 11199: Runtime Interface Emulator for local only; enable the function
    rie (emulator) for local only; enable the function

--- deploy/modules/sourcehub/sourcehub-taskworker-sync.tf
+++ deploy/modules/sourcehub/sourcehub-taskworker-sync.tf
@@ -120,7 +120,7 @@ resource "aws_iam_policy" "sourcehub-taskworker-sync-function-basic" {
 
 resource "aws_lambda_event_source_mapping" "sourcehub-taskworker-trigger" {
   event_source_arn = aws_sqs_queue.taskworker_dlq.arn
-  enabled          = false
+  enabled          = true
   function_name    = local.function_name
   batch_size       = 1
 
