6265f1c95006b9ee756087241e058ef412b7af4c
Jeff Loiola <jloiola@mmitnetwork.com>
2023-02-22 12:27:35 +0000

    Merged PR 11197: Match SQS and lambda timeouts
    Match SQS and lambda timeouts

--- deploy/modules/sourcehub/sourcehub-taskworker.tf
+++ deploy/modules/sourcehub/sourcehub-taskworker.tf
@@ -196,8 +196,9 @@ resource "aws_sqs_queue" "taskworker" {
 }
 
 resource "aws_sqs_queue" "taskworker_dlq" {
-  name       = format("%s-%s-%s-taskworker-%s-mmit-sqs-%02d-dlq.fifo", local.app_name, var.environment, local.service_name, local.short_region, var.revision)
-  fifo_queue = true
+  name                       = format("%s-%s-%s-taskworker-%s-mmit-sqs-%02d-dlq.fifo", local.app_name, var.environment, local.service_name, local.short_region, var.revision)
+  fifo_queue                 = true
+  visibility_timeout_seconds = 60
   tags = merge(local.effective_tags, {
     component = "${local.service_name}-taskworker"
   })
