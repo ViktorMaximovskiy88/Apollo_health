035c98333bdcdaee9662bd4c4e91724d68c67328
Jeremy Aldrich <jaldrich@mmitnetwork.com>
2023-02-22 16:18:55 -0500

    [APH-1179] added spinner

--- frontend/src/features/doc_documents/DocDocumentsPage.tsx
+++ frontend/src/features/doc_documents/DocDocumentsPage.tsx
@@ -1,35 +1,45 @@
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { Spin } from 'antd';
+
 import { DocDocumentsDataTable } from './DocDocumentsDataTable';
 import { MainLayout } from '../../components';
 import { DocTypeUpdateModal } from './DocTypeBulkUpdateModal';
-import { useSelector } from 'react-redux';
 import {
   docDocumentTableState,
   setDocDocumentTableForceUpdate,
   setDocDocumentTableSelect,
 } from './docDocumentsSlice';
-import { useCallback } from 'react';
 import { useAppDispatch } from '../../app/store';
+import { useLazyGetDocDocumentsQuery } from './docDocumentApi';
 
-function SectionToolbar() {
+function SectionToolbar(props: { isFetching: boolean }) {
+  const { isFetching } = props;
   const tableState = useSelector(docDocumentTableState);
   const dispatch = useAppDispatch();
   const onBulkSubmit = useCallback(() => {
     dispatch(setDocDocumentTableForceUpdate());
     dispatch(setDocDocumentTableSelect({ selected: {}, unselected: {} }));
   }, [dispatch, setDocDocumentTableForceUpdate, setDocDocumentTableSelect]);
+
   return (
-    <DocTypeUpdateModal
-      selection={tableState.selection}
-      filterValue={tableState.filter}
-      onBulkSubmit={onBulkSubmit}
-    />
+    <>
+      <Spin spinning={isFetching} />
+      <DocTypeUpdateModal
+        selection={tableState.selection}
+        filterValue={tableState.filter}
+        onBulkSubmit={onBulkSubmit}
+      />
+    </>
   );
 }
 
 export function DocDocumentsPage() {
+  const [getDocDocumentsFn, { isFetching }] = useLazyGetDocDocumentsQuery();
+
   return (
-    <MainLayout sectionToolbar={<SectionToolbar />}>
-      <DocDocumentsDataTable />
+    <MainLayout sectionToolbar={<SectionToolbar isFetching={isFetching} />}>
+      <DocDocumentsDataTable getDocDocumentsFn={getDocDocumentsFn} />
     </MainLayout>
   );
 }
